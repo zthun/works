7fd44806c16d4d55fc471d341ab39490b7ecc429
Anthony Bonta <zthun.coding@gmail.com>
2019-08-18 16:07:21 -0500

    More cleanup.  Updated to latest dependencies.

--- jest.config.js
+++ jest.config.js
@@ -11,11 +11,15 @@ module.exports = {
   },
   modulePathIgnorePatterns: [
     '<rootDir>/zauth.core/dist',
-    '<rootDir>/zauth.angular/dist'
+    '<rootDir>/zauth.angular/dist',
+    '<rootDir>/zauth.vue/dist',
+    '<rootDir>/zauth.react/dist'
   ],
   moduleNameMapper: {
     '@zthun/auth.core': '<rootDir>/zauth.core/src/index',
-    '@zthun/auth.angular': '<rootDir>/zauth.angular/src/index'
+    '@zthun/auth.angular': '<rootDir>/zauth.angular/src/index',
+    '@zthun/auth.vue': '<rootDir>/zauth.vue/src/index',
+    '@zthun/auth.react': '<rootDir>/zauth.react/src/index'
   },
   coverageDirectory: '../reports/coverage'
 };
