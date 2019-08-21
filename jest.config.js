e4e441ff5de7ab98fce5b76e9a749ec2c7fc0ac9
Anthony Bonta <zthun.coding@gmail.com>
2019-08-20 20:57:48 -0500

    Removed vue. It's just not really where I want it to be when using TypeScript.

--- jest.config.js
+++ jest.config.js
@@ -7,24 +7,22 @@ module.exports = {
   rootDir: 'packages',
   testRegex: '.spec.ts$',
   transform: {
-    '^.+\\.(t|j)s$': 'ts-jest',
-    '^.+\\.vue$': 'vue-jest'
+    '^.+\\.(t|j)s$': 'ts-jest'
   },
   moduleFileExtensions: [
     'js',
+    'jsx',
     'ts',
-    'vue'
+    'tsx'
   ],
   modulePathIgnorePatterns: [
     '<rootDir>/zauth.core/dist',
     '<rootDir>/zauth.angular/dist',
-    '<rootDir>/zauth.vue/dist',
     '<rootDir>/zauth.react/dist'
   ],
   moduleNameMapper: {
     '@zthun/auth.core': '<rootDir>/zauth.core/src/index',
     '@zthun/auth.angular': '<rootDir>/zauth.angular/src/index',
-    '@zthun/auth.vue': '<rootDir>/zauth.vue/src/index',
     '@zthun/auth.react': '<rootDir>/zauth.react/src/index'
   },
   coverageDirectory: '../reports/coverage'
