81771239225bb6953d6c3d57f18f0e9be21b2bdd
Anthony Bonta <zthun.coding@gmail.com>
2019-08-23 00:12:44 -0500

    Updated the login form to show the icon.

--- packages/zauth.client/src/login/login-form.tsx
+++ packages/zauth.client/src/login/login-form.tsx
@@ -1,11 +1,17 @@
-import { Card, CardContent, CardHeader, TextField, Button } from '@material-ui/core';
+import { faSignInAlt } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { Button, Card, CardContent, CardHeader, TextField, Typography } from '@material-ui/core';
 import * as React from 'react';
 
 export class ZLoginForm extends React.Component {
   public render() {
     return (
       <Card>
-        <CardHeader title='Existing user?' subheader='Enter your credentials' />
+        <CardHeader
+          title='Existing user?'
+          subheader='Enter your credentials'
+          avatar={<FontAwesomeIcon icon={faSignInAlt} size='3x' />}
+        />
         <CardContent>
           <form noValidate={true} autoComplete='off'>
             <TextField className='mb-md' fullWidth={true} label='Email' type='email' margin='none' variant='outlined' />
