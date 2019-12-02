8a783e96c35e6ac849ee97489af0c2aaa184f1b3
Anthony Bonta <zthun.coding@gmail.com>
2019-12-02 11:50:00 -0600

    Fixed unit tests.

--- packages/zauth.client/src/new-user/new-user-page.component.spec.tsx
+++ packages/zauth.client/src/new-user/new-user-page.component.spec.tsx
@@ -1,16 +1,30 @@
 
-import { configure, shallow } from 'enzyme';
+import { configure, mount, ReactWrapper } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+import { createMemoryHistory, MemoryHistory } from 'history';
 import React from 'react';
+import { Router } from 'react-router-dom';
 import { ZNewUserPage } from './new-user-page.component';
 
 describe('ZNewUserPage', () => {
+  let history: MemoryHistory;
+  let _target: ReactWrapper;
+
   function createTestTarget() {
-    return shallow(<ZNewUserPage />);
+    _target = mount(<Router history={history}><ZNewUserPage /></Router>);
+    return _target;
   }
 
   beforeAll(() => {
     configure({ adapter: new Adapter() });
+  });
+
+  beforeEach(() => {
+    history = createMemoryHistory();
+  });
+
+  afterEach(() => {
+    _target.unmount();
   });
 
   it('renders the page', () => {
