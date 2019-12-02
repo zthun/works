b72fb08934683122e09d9bf4a7c93c561dfb9211
Anthony Bonta <zthun.coding@gmail.com>
2019-12-02 11:46:21 -0600

    Added the flow for creating a user.

--- packages/zauth.client/src/new-user/new-user-page.component.tsx
+++ packages/zauth.client/src/new-user/new-user-page.component.tsx
@@ -1,12 +1,31 @@
-import { IZLogin } from '@zthun/auth.core';
+import { IZLogin, IZToken, IZUser, ZUrlBuilder } from '@zthun/auth.core';
+import Axios from 'axios';
 import React from 'react';
+import { useHistory } from 'react-router';
 import { ZNewUserForm } from './new-user-form.component';
 
 export function ZNewUserPage() {
-  function handleCreate(login: IZLogin): Promise<void> {
-    return new Promise((resolve) => {
-      setTimeout(resolve, 2000);
-    });
+  const history = useHistory();
+
+  async function handleCreate(login: IZLogin): Promise<void> {
+    const newUserUrl = new ZUrlBuilder().location().subdomain('api').append('users').build();
+    const user = await Axios.post<IZUser>(newUserUrl, login);
+
+    if (user.status !== 200) {
+      // Error
+      return;
+    }
+
+    const loginUrl = new ZUrlBuilder().location().subdomain('api').append('tokens').build();
+    const token = await Axios.post<IZToken>(loginUrl, login);
+
+    if (user.status !== 200) {
+      // Error
+      return;
+    }
+
+    // Save the token.
+    history.push('profile');
   }
 
   return (
---
+++ packages/zauth.core/src/token/token.interface.ts
@@ -0,0 +1,4 @@
+export interface IZToken {
+  token: string;
+  userId: string;
+}
