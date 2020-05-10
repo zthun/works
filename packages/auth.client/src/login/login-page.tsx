fe5d855baa67c4995a16fdeb7256a2a11c62fb2f
Anthony Bonta <anthony.bonta@trintech.com>
2020-05-09 22:21:03 -0500

    Fixing lint.

--- packages/auth.client/src/login/login-page.tsx
+++ packages/auth.client/src/login/login-page.tsx
@@ -2,12 +2,6 @@ import { ZLoginTabs } from '@zthun/auth.react';
 import React from 'react';
 
 export function ZLoginPage() {
-  function handleLogin() {}
-
-  function handleCreate() {}
-
-  function handleRecover() {}
-
   return (
     <div className='ZLoginPage-root mx-auto w-50 mt-em-5' data-testid='ZLoginPage-root'>
       <ZLoginTabs />
