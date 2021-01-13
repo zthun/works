eacff3f635212280892dff84de734712de29b7c0
Anthony Bonta <zthun.coding@gmail.com>
2021-01-13 16:19:37 -0600

    Added the name, icon, type, comment short text and full text.

--- packages/works.react/src/doc/typedoc-entity-viewer.tsx
+++ packages/works.react/src/doc/typedoc-entity-viewer.tsx
@@ -13,16 +13,40 @@ import { ZTypedocIcon } from './typedoc-icon';
  * @returns The jsx that displays the entity information.
  */
 export function ZTypedocEntityViewer(props: IZTypedocEntityViewerProps) {
+  /**
+   * Creates the tsx for the comment paragraphs.
+   *
+   * @returns The tsx for the comment fragment.
+   */
+  function createComment() {
+    if (!props.entity.comment) {
+      return null;
+    }
+
+    const short = props.entity.comment.shortText ? (
+      <Typography className='ZTypedocEntityViewer-comment-shortText' variant='body2'>
+        {props.entity.comment.shortText}
+      </Typography>
+    ) : null;
+
+    const long = props.entity.comment.text ? (
+      <Typography className='ZTypedocEntityViewer-comment-text' variant='body2'>
+        {props.entity.comment.text}
+      </Typography>
+    ) : null;
+
+    return (
+      <Fragment>
+        {short}
+        {long}
+      </Fragment>
+    );
+  }
   return (
     <Fragment>
       <div className='ZTypedocEntityViewer-root' data-testid='ZTypedocEntityViewer-root'>
         <ZPaperCard headerText={props.entity.name} action={props.action} avatar={<ZTypedocIcon kind={props.entity.kind} size='md' />} subHeaderText={props.entity.kindString} size='lg'>
-          <Typography className='ZTypedocEntityViewer-comment-shortText' variant='body2'>
-            {props.entity.comment.shortText}
-          </Typography>
-          <Typography className='ZTypedocEntityViewer-comment-text' variant='body2'>
-            {props.entity.comment.text}
-          </Typography>
+          {createComment()}
         </ZPaperCard>
       </div>
     </Fragment>
