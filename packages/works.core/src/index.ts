03415a1df0a7de90a657e53cd3fb4f7600b685c6
Anthony Bonta <zthun.coding@gmail.com>
2021-02-14 17:23:48 -0600

    Removed ZTypedocSample.

--- packages/works.core/src/index.ts
+++ packages/works.core/src/index.ts
@@ -14,7 +14,6 @@ export { IZTypedocGroup } from './doc/typedoc-group.interface';
 export { ZTypedocKind } from './doc/typedoc-kind.enum';
 export { IZTypedocSource } from './doc/typedoc-source.interface';
 export { ZTypedocTypeKind } from './doc/typedoc-type-kind.enum';
-export { IZTypedocSampleData, IZTypedocSampleDataType, ZTypedocSample, ZTypedocSampleB, ZSampleMatrixPrefix, ZSampleMatrixSuffix } from './doc/typedoc-sample';
 export { IZTypedocType } from './doc/typedoc-type.interface';
 export { IZTypedoc } from './doc/typedoc.interface';
 // Email
