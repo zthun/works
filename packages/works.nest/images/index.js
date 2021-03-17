ecb58b5d84eeaec8498652ff7fcdddc55f1ad833
Anthony Bonta <zthun.coding@gmail.com>
2021-03-17 00:31:36 -0500

    Exporting images.

--- packages/works.nest/images/index.js
+++ packages/works.nest/images/index.js
@@ -1,2 +1,8 @@
+// PNG
+require('./png/works.nest.auth.png');
+require('./png/works.nest.notifications.png');
+require('./png/works.nest.users.png');
+require('./png/works.nest.vault.png');
+
 // SVG
 require('./svg/works.nest.svg');
