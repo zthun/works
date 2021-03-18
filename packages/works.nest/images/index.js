5783b709a67e38957dc37e6ad0c6aeaa66e96fad
Anthony Bonta <zthun.coding@gmail.com>
2021-03-17 23:19:18 -0500

    Added the health icon.

--- packages/works.nest/images/index.js
+++ packages/works.nest/images/index.js
@@ -1,5 +1,6 @@
 // PNG
 require('./png/works.nest.auth.png');
+require('./png/works.nest.health.png');
 require('./png/works.nest.notifications.png');
 require('./png/works.nest.users.png');
 require('./png/works.nest.vault.png');
