13102c5af78069577b6ecdf1c0cd66ab6ca23fd4
Anthony Bonta <zthun.coding@gmail.com>
2021-05-26 17:14:19 -0500

    test: updated jest configuration

--- jest.config.ts
+++ jest.config.ts
@@ -1,11 +1,12 @@
-const { pathsToModuleNameMapper } = require('ts-jest/utils');
-const { compilerOptions } = require('./tsconfig.json');
+import { pathsToModuleNameMapper } from 'ts-jest/utils';
+import { compilerOptions } from './tsconfig.json';
 
-module.exports = {
+export default {
   rootDir: compilerOptions.baseUrl,
   testTimeout: 60000,
   testRegex: '.spec.(ts|tsx)$',
   transform: { '^.+\\.(ts|tsx)$': 'ts-jest' },
+  testEnvironment: 'jsdom',
   moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx'],
   moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, { prefix: '<rootDir>/' }),
   coverageDirectory: '../reports/coverage'
