5a9b2dcf72215811a78a764627c333a3505be4dc
Anthony Bonta <zthun.coding@gmail.com>
2021-05-27 13:29:12 -0500

    refactor: revert the markdown allowed elements

--- packages/works.react/src/doc/markdown-viewer.tsx
+++ packages/works.react/src/doc/markdown-viewer.tsx
@@ -62,7 +62,7 @@ export function ZMarkdownViewer(props: IZMarkdownViewerProps) {
       onAction={props.onAction}
     >
       <div ref={markdownEl}>
-        <ReactMarkdown className='ZMarkdownViewer-markdown' plugins={[gfm]} linkTarget='_blank' allowedElements={['img', 'p']}>
+        <ReactMarkdown className='ZMarkdownViewer-markdown' plugins={[gfm]} linkTarget='_blank'>
           {markdown}
         </ReactMarkdown>
       </div>
