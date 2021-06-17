bf4e579427bdd0bc7e4c376827f3878db0470e95
Anthony Bonta <zthun.coding@gmail.com>
2021-06-17 10:26:44 -0500

    test: updated test coverage

--- packages/works.client/src/app/works-app.spec.tsx
+++ packages/works.client/src/app/works-app.spec.tsx
@@ -1,8 +1,10 @@
+/* eslint-disable require-jsdoc */
 import { render } from '@testing-library/react';
 import { IZProfile, ZProfileBuilder } from '@zthun/works.core';
-import Axios from 'axios';
+import Axios, { AxiosResponse } from 'axios';
 import React from 'react';
-import { ZthunworksApp } from './works-app';
+import { RouteComponentProps } from 'react-router-dom';
+import { renderPrivacyPage, renderStatusCodePage, renderTermsPage, ZthunworksApp } from './works-app';
 
 jest.mock('axios');
 
@@ -22,5 +24,30 @@ describe('ZthunworksApp', () => {
     const actual = target.queryByTestId('Zthunworks-root');
     // Assert
     expect(actual).toBeTruthy();
+  });
+
+  describe('Render', () => {
+    function assertRendersPage<T = any>(renderFn: (p?: RouteComponentProps<T>) => JSX.Element, p?: RouteComponentProps<T>) {
+      // Arrange
+      // Act
+      const target = renderFn(p);
+      // Assert
+      expect(target).toBeTruthy();
+    }
+
+    it('should return the terms page.', () => {
+      (Axios.get as unknown as jest.Mock<AxiosResponse<string>>).mockReturnValue({ data: '', status: 200, statusText: 'Success', headers: [], config: null });
+      assertRendersPage(renderTermsPage);
+    });
+
+    it('should render the privacy page.', () => {
+      (Axios.get as unknown as jest.Mock<AxiosResponse<string>>).mockReturnValue({ data: '', status: 200, statusText: 'Success', headers: [], config: null });
+      assertRendersPage(renderPrivacyPage);
+    });
+
+    it('should render the status code page.', () => {
+      const props = { match: { params: { code: '404' }, isExact: true, path: 'status-code/:code', url: null } } as RouteComponentProps<any>;
+      assertRendersPage(renderStatusCodePage, props);
+    });
   });
 });
--- packages/works.client/src/app/works-app.tsx
+++ packages/works.client/src/app/works-app.tsx
@@ -6,7 +6,7 @@ import { ZAlertStack, ZAlertStackContext, ZAlertStackList, ZDataState, ZLoginSta
 import { ZUrlBuilder } from '@zthun/works.url';
 import Axios from 'axios';
 import React from 'react';
-import { HashRouter, Redirect, Route, Switch } from 'react-router-dom';
+import { HashRouter, Redirect, Route, RouteComponentProps, Switch } from 'react-router-dom';
 import { ZHomePage } from '../home/home-page';
 import { ZApiPage } from '../learn/api-page';
 import { ZLearnPage } from '../learn/learn-page';
@@ -14,8 +14,44 @@ import { ZLoginPage } from '../login/login-page';
 import { ZthunworksMenu } from '../menu/works-menu';
 import { ZProfilePage } from '../profile/profile-page';
 
+/**
+ * The url to the terms markdown.
+ */
 export const ZUrlMarkdownTerms = new ZUrlBuilder().location().hash('').path('legal/TERMS.md').build();
+
+/**
+ * The url to the privacy markdown.
+ */
 export const ZUrlMarkdownPrivacy = new ZUrlBuilder().location().hash('').path('legal/PRIVACY.md').build();
+
+/**
+ * Returns the jsx for the privacy page render.
+ *
+ * @returns The jsx for the privacy page.
+ */
+export function renderPrivacyPage() {
+  return <ZMarkdownPage src={ZUrlMarkdownPrivacy} headerText='Privacy' subHeaderText='Information collection' avatar={<InfoIcon fontSize='large' />} size='lg' />;
+}
+
+/**
+ * Returns the jsx for the terms page render.
+ *
+ * @returns The jsx for the terms page.
+ */
+export function renderTermsPage() {
+  return <ZMarkdownPage src={ZUrlMarkdownTerms} headerText='Terms' subHeaderText='Usage of this website' avatar={<MouseIcon fontSize='large' />} size='lg' />;
+}
+
+/**
+ * Renders the jsx for the status code page.
+ *
+ * @param props The render props.
+ *
+ * @returns The jsx for the status code page.
+ */
+export function renderStatusCodePage(props: RouteComponentProps<{ code: string }>) {
+  return <ZStatusCodePage code={props.match.params.code} />;
+}
 
 /**
  * Represents the entry point of the client application.
@@ -45,9 +81,9 @@ export function ZthunworksApp() {
                 <Route exact path='/home' component={ZHomePage} />
                 <Route exact path='/login' component={ZLoginPage} />
                 <Route exact path='/profile' component={ZProfilePage} />
-                <Route exact path='/privacy' render={() => <ZMarkdownPage src={ZUrlMarkdownPrivacy} headerText='Privacy' subHeaderText='Information collection' avatar={<InfoIcon fontSize='large' />} size='lg' />} />
-                <Route exact path='/terms' render={() => <ZMarkdownPage src={ZUrlMarkdownTerms} headerText='Terms' subHeaderText='Usage of this website' avatar={<MouseIcon fontSize='large' />} size='lg' />} />
-                <Route exact path='/status-code/:code' render={(p) => <ZStatusCodePage code={p.match.params.code} />} />
+                <Route exact path='/privacy' render={renderPrivacyPage} />
+                <Route exact path='/terms' render={renderTermsPage} />
+                <Route exact path='/status-code/:code' render={renderStatusCodePage} />
                 <Route exact path='/learn/:pkg' component={ZLearnPage} />
                 <Route exact path='/learn/:pkg/api' component={ZApiPage} />
                 <Route exact path='/learn/:pkg/api/:enid' component={ZApiPage} />
