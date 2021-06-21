3cfbf62d5ac1ee780e4ae6c2b6dd3eeae25e900a
Anthony Bonta <zthun.coding@gmail.com>
2021-06-21 15:39:45 -0500

    test: update unit tests for data state

--- packages/works.client/src/menu/works-menu.spec.tsx
+++ packages/works.client/src/menu/works-menu.spec.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable require-jsdoc */
 import { fireEvent, render } from '@testing-library/react';
 import { IZProfile, ZProfileBuilder } from '@zthun/works.core';
-import { IZAlertStack, IZDataState, ZAlertStack, ZAlertStackContext, ZDataStateStatic, ZLoginStateContext } from '@zthun/works.react';
+import { IZAlertStack, IZDataState, ZAlertStack, ZAlertStackContext, ZDataState, ZLoginStateContext } from '@zthun/works.react';
 import Axios from 'axios';
 import { createMemoryHistory, MemoryHistory } from 'history';
 import React from 'react';
@@ -30,7 +30,7 @@ describe('ZthunworksMenu', () => {
 
   beforeEach(() => {
     history = createMemoryHistory();
-    loginState = new ZDataStateStatic<IZProfile>();
+    loginState = new ZDataState<IZProfile>();
     alerts = new ZAlertStack();
 
     (Axios.delete as jest.Mock).mockClear();
@@ -39,7 +39,7 @@ describe('ZthunworksMenu', () => {
   describe('Profile state', () => {
     describe('Logged out.', () => {
       beforeEach(() => {
-        loginState = new ZDataStateStatic<IZProfile>(null);
+        loginState = new ZDataState<IZProfile>(null);
       });
 
       it('should move to the login page when the profile menu is clicked.', async () => {
@@ -60,7 +60,7 @@ describe('ZthunworksMenu', () => {
 
       beforeEach(() => {
         menu = 'Gambit';
-        loginState = new ZDataStateStatic(new ZProfileBuilder().email('gambit@marvel.com').display(menu).build());
+        loginState = new ZDataState(new ZProfileBuilder().email('gambit@marvel.com').display(menu).build());
       });
 
       it('should move to the profile page when the profile menu item is clicked.', async () => {
@@ -74,37 +74,6 @@ describe('ZthunworksMenu', () => {
         // Assert
         expect(history.location.pathname).toEqual('/profile');
       });
-
-      /**
-       * TODO:  This needs to be moved to the profile page.
-      it('should log out the user when the logout menu item is clicked.', async () => {
-        // Arrange
-        const target = await createTestTarget();
-        // Act
-        await act(async () => openMenu(target));
-        await act(async () => {
-          const logoutMenuItem = target.getByText('LOGOUT');
-          fireEvent.click(logoutMenuItem);
-        });
-        // Assert
-        expect(Axios.delete).toHaveBeenCalledWith(expect.stringContaining('tokens'));
-      });
-
-      it('should notify the user that an error has occurred when logging out.', async () => {
-        // Arrange
-        const target = await createTestTarget();
-        const error = 'Can not log out.';
-        (Axios.delete as jest.Mock).mockRejectedValue(error);
-        // Act
-        await act(async () => openMenu(target));
-        await act(async () => {
-          const logoutMenuItem = target.getByText('LOGOUT');
-          fireEvent.click(logoutMenuItem);
-        });
-        // Assert
-        expect(alerts.list[0].message).toEqual(error);
-      });
-      */
     });
   });
 });
