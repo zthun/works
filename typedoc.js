001290fbd1ea12fede5832292e86450862367e78
Anthony Bonta <zthun.coding@gmail.com>
2021-08-06 16:18:51 -0500

    fix: typedoc should now be properly generated

--- typedoc.js
+++ typedoc.js
@@ -1,7 +1,6 @@
 module.exports = {
   out: 'docs/typedoc',
   json: 'docs/typedoc.json',
-  exclude: ['src/index.ts'],
   excludePrivate: true,
   tsconfig: 'tsconfig.esm.json',
   entryPoints: ['src/index.ts']
