9d851e706107bca5131415836170fb6c08027618
Anthony Bonta <zthun.coding@gmail.com>
2021-08-15 17:56:02 -0500

    refactor: combined the interface and class to the context

--- packages/works.react/src/file/file-select.context.ts
+++ packages/works.react/src/file/file-select.context.ts
@@ -1,6 +1,72 @@
 import { createContext, useContext } from 'react';
-import { ZFileSelectInput } from './file-select-input.class';
-import { IZFileSelect } from './file-select.interface';
+
+/**
+ * A service that allows the user to open a file.
+ *
+ * Because of the limitations of how files get opened,
+ * we have to use callbacks instead of promises in the case that the
+ * user cancels.  If the user cancels, then we don't want to wind up
+ * with a memory leak with the promise.
+ */
+export interface IZFileSelect {
+  /**
+   * Opens the file select dialog.
+   *
+   * @param filter The file filter.
+   * @param cb The callback if the user accepts the file.
+   */
+  open(accept: string, cb: (file: File) => void): void;
+}
+
+/**
+ * Represents a file select that is opened from an input.
+ */
+export class ZFileSelectInput implements IZFileSelect {
+  private _input: HTMLInputElement;
+  private _cb: (file: File) => void;
+
+  /**
+   * Initializes a new instance of this object.
+   *
+   * @param doc The root document that can be used to create the file dialog.
+   * @param container The container to house the input element.
+   */
+  public constructor(doc: Document = document, container: HTMLElement = document.body) {
+    this._input = doc.createElement('input');
+    this._input.type = 'file';
+    this._input.style.position = 'absolute';
+    this._input.style.visibility = 'hidden';
+    this._input.style.width = '1px';
+    this._input.style.height = '1px';
+    this._input.style.left = '0';
+    this._input.style.top = '0';
+    this._input.style.zIndex = '-1';
+    this._input.onchange = () => {
+      if (this._input.files[0]) {
+        this._cb(this._input.files[0]);
+      }
+    };
+    container.appendChild(this._input);
+  }
+
+  /**
+   * Opens the file dialog.
+   *
+   * Note that this uses the older node callback style rather than returning a promise.
+   * This is intentional due to how this object opens a file select.  Since there is no
+   * way to determine if the user canceled the dialog, returning a promise would actually
+   * potentially result in a memory leak since it would never get resolved.  Therefore,
+   * the class callback style has to be used here.
+   *
+   * @param accept The file mime types to accept.
+   * @param cb The callback for when the user selects a file.
+   */
+  public open(accept: string, cb: (file: File) => void) {
+    this._input.accept = accept;
+    this._cb = cb;
+    this._input.click();
+  }
+}
 
 /**
  * Represents the context for using a file select object.
