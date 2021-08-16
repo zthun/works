f665f2d9c0d93854adf5e93b3199a746278e0898
Anthony Bonta <zthun.coding@gmail.com>
2021-08-15 19:28:01 -0500

    refactor: rename to window service

--- packages/works.react/src/window/window-service.context.ts
+++ packages/works.react/src/window/window-service.context.ts
@@ -3,13 +3,13 @@ import { createContext, useContext } from 'react';
 /**
  * Represents the window context.
  */
-export const ZWindowContext = createContext<Window>(window);
+export const ZWindowServiceContext = createContext<Window>(window);
 
 /**
  * Gets the current window object.
  *
  * @returns The current window object.
  */
-export function useWindow() {
-  return useContext(ZWindowContext);
+export function useWindowService() {
+  return useContext(ZWindowServiceContext);
 }
