582a3e1e107b1a7bdbcdb39db5284389433bd91c
Anthony Bonta <zthun.coding@gmail.com>
2021-08-30 18:14:07 -0500

    chore: export the new error handler and message handler

--- packages/works.error/src/index.ts
+++ packages/works.error/src/index.ts
@@ -3,7 +3,8 @@
 // Error
 export { IZError, ZErrorBuilder } from './error/error';
 // Handler
-export { IZErrorHandler } from './handler/error-handler';
+export { IZErrorHandler, ZErrorHandler } from './handler/error-handler';
+export { IZMessageHandler } from './handler/message-handler';
 // Pass Through
 export { IZErrorPassThrough, ZErrorPassThrough } from './pass-through/error-pass-through';
 // Recovery
