d086b661bc6c0a5d27971de82dd33912790a2db3
Anthony Bonta <zthun.coding@gmail.com>
2021-08-30 18:13:52 -0500

    feat: added the standard error handler that unwraps messages

---
+++ packages/works.error/src/handler/error-handler.spec.ts
@@ -0,0 +1,159 @@
+/* eslint-disable require-jsdoc */
+import { createMocked } from '@zthun/works.jest';
+import { ZErrorHandler } from './error-handler';
+import { IZMessageHandler } from './message-handler';
+
+describe('ErrorHandler', () => {
+  let msg: IZMessageHandler;
+
+  function createTestTarget() {
+    return new ZErrorHandler(msg);
+  }
+
+  beforeEach(() => {
+    msg = createMocked<IZMessageHandler>(['handle']);
+  });
+
+  function assertMessageHandlerReceivesMessage(expected: string, err: any) {
+    // Arrange
+    const target = createTestTarget();
+    // Act
+    target.handle(err);
+    // Assert
+    expect(msg.handle).toHaveBeenCalledWith(expected, err);
+  }
+
+  describe('Primitives', () => {
+    describe('String', () => {
+      it('should pass the message directly to the message handler.', () => {
+        assertMessageHandlerReceivesMessage('Message', 'Message');
+      });
+    });
+
+    describe('Number', () => {
+      it('should pass the message directly to the message handler.', () => {
+        assertMessageHandlerReceivesMessage('42', 42);
+      });
+    });
+
+    describe('Boolean', () => {
+      it('should pass the true string directly to the message handler.', () => {
+        assertMessageHandlerReceivesMessage('true', true);
+      });
+
+      it('should pass the false string directly to the message handler.', () => {
+        assertMessageHandlerReceivesMessage('false', false);
+      });
+    });
+
+    describe('Symbol', () => {
+      it('should pass the number description directly to the message handler.', () => {
+        assertMessageHandlerReceivesMessage('100', Symbol(100));
+      });
+
+      it('should pass the string description directly to the message handler.', () => {
+        assertMessageHandlerReceivesMessage('msg', Symbol('msg'));
+      });
+    });
+
+    describe('BigInt', () => {
+      it('should pass the number directly to the message handler.', () => {
+        assertMessageHandlerReceivesMessage('12345678987654321', BigInt('12345678987654321'));
+      });
+    });
+  });
+
+  describe('No Error', () => {
+    it('should not invoke the message handler with an undefined error.', () => {
+      // Arrange
+      const target = createTestTarget();
+      // Act
+      target.handle(undefined);
+      // Assert
+      expect(msg.handle).not.toHaveBeenCalled();
+    });
+
+    it('should not invoke the message handler with a null error.', () => {
+      // Arrange
+      const target = createTestTarget();
+      // Act
+      target.handle(undefined);
+      // Assert
+      expect(msg.handle).not.toHaveBeenCalled();
+    });
+  });
+
+  describe('Object', () => {
+    describe('Known Shape', () => {
+      describe('ZError', () => {
+        it('should return the english message if a code and an english property is on the object.', () => {
+          const err = { code: 237, english: 'This is the shape of a ZError found from the works.error package.' };
+          assertMessageHandlerReceivesMessage(err.english, err);
+        });
+
+        it('should return the code if the english message is not available.', () => {
+          const err = { code: 237 };
+          assertMessageHandlerReceivesMessage(`${err.code}`, err);
+        });
+      });
+
+      describe('ZHttpResult', () => {
+        it('should return the message information from the data object.', () => {
+          const err = { data: 'This is an error message in the shape of a ZHttpResult found in works.html.' };
+          assertMessageHandlerReceivesMessage(err.data, err);
+        });
+      });
+
+      describe('Object with message', () => {
+        it('should just send the message property.', () => {
+          const err = { message: 'message' };
+          assertMessageHandlerReceivesMessage(err.message, err);
+        });
+      });
+    });
+
+    describe('Composition', () => {
+      it('should send and unwrap the lowest level message.', () => {
+        const err = {
+          status: 404,
+          data: {
+            message: {
+              code: 404,
+              english: 'The underlying error message'
+            }
+          }
+        };
+        assertMessageHandlerReceivesMessage(err.data.message.english, err);
+      });
+    });
+
+    describe('Unknown Shape', () => {
+      it('should send the stringified json to the message handler.', () => {
+        const obj = { one: '1', two: '2', three: { threePointOne: '3.1', threePointTwo: '3.2' } };
+        const expected = JSON.stringify(obj, undefined, ZErrorHandler.Spacing);
+        assertMessageHandlerReceivesMessage(expected, obj);
+      });
+
+      it('should send the stringified json to the message handler if the final unwrapped message cannot be found.', () => {
+        const err = {
+          status: 404,
+          data: {
+            message: {
+              mystery: 'Not a message but will still show up as json.'
+            }
+          }
+        };
+        const expected = JSON.stringify(err.data.message, undefined, ZErrorHandler.Spacing);
+        assertMessageHandlerReceivesMessage(expected, err);
+      });
+    });
+  });
+
+  describe('Function', () => {
+    it('should return the message from the function return value.', () => {
+      const expected = 'Message';
+      const fn = () => expected;
+      assertMessageHandlerReceivesMessage(expected, fn);
+    });
+  });
+});
--- packages/works.error/src/handler/error-handler.ts
+++ packages/works.error/src/handler/error-handler.ts
@@ -1,3 +1,5 @@
+import { IZMessageHandler } from './message-handler';
+
 /**
  * Represents an object that can be used to handle an error.
  *
@@ -13,4 +15,105 @@ export interface IZErrorHandler {
    * @param err The error to handle.
    */
   handle(err: any): void;
+}
+
+/**
+ * Represents the default implementation of the IZErrorHandler.
+ *
+ * The default implementation attempts to figure out the shape
+ * of the error and send an actual message to a message handler.
+ */
+export class ZErrorHandler implements IZErrorHandler {
+  /**
+   * Represents the spacing when converting to JSON.
+   */
+  public static Spacing = '  ';
+
+  /**
+   * Initializes a new instance of this object.
+   *
+   * @param _msg The handler that will receive the message
+   *            stripped out from the error.
+   */
+  public constructor(private _msg: IZMessageHandler) {}
+
+  /**
+   * Attempts to unwrap the message from the error object.
+   *
+   * @param err The error to unwrap.
+   *
+   * @returns The unwrapped message or null if the message could not be detected.
+   */
+  private _unwrapMessage(err: any) {
+    let msg = null;
+
+    if (Object.hasOwnProperty.call(err, 'code')) {
+      // This may be a proper ZError.  Try to extract a message.
+      msg = this._findMessage(err.english || err.code);
+    }
+
+    if (msg == null && Object.hasOwnProperty.call(err, 'data')) {
+      // This is probably an http object - either from axios or the works.http package result.
+      msg = this._findMessage(err.data);
+    }
+
+    if (msg == null && Object.hasOwnProperty.call(err, 'message')) {
+      // An object with a literal message.
+      msg = this._findMessage(err.message);
+    }
+
+    return msg;
+  }
+
+  /**
+   * Attempts to build a message from an object.
+   *
+   * @param err The err object to determine the message.
+   *
+   * @returns The message parsed from err.
+   */
+  private _findMessageFromShape(err: object) {
+    const message = this._unwrapMessage(err);
+    return message == null ? JSON.stringify(err, undefined, ZErrorHandler.Spacing) : message;
+  }
+
+  /**
+   * Attempts to build a message.
+   *
+   * @param err The error to build a message from.
+   *
+   * @returns A parsed error message from err.
+   */
+  private _findMessage(err: any) {
+    if (err == null) {
+      return err;
+    }
+
+    if (typeof err === 'function') {
+      return this._findMessage(err());
+    }
+
+    if (typeof err === 'object') {
+      return this._findMessageFromShape(err);
+    }
+
+    if (typeof err === 'symbol') {
+      return err.description;
+    }
+
+    return `${err}`;
+  }
+
+  /**
+   * Handles and error.
+   *
+   * @param err The error to handle.
+   */
+  public handle(err: any): void {
+    const msg = this._findMessage(err);
+
+    if (msg) {
+      this._msg.handle(msg, err);
+    }
+  }
 }
