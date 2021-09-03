a4b3493e470465beae93a998968b574497e3c85a
Anthony Bonta <zthun.coding@gmail.com>
2021-09-02 21:12:52 -0500

    refactor: paper card no longer uses default props
    It uses TypeScript for validations instead

--- packages/works.react/src/card/paper-card.tsx
+++ packages/works.react/src/card/paper-card.tsx
@@ -1,8 +1,21 @@
 import { Button, Card, CardActions, CardContent, CardHeader, CardMedia, Checkbox, FormControlLabel, Paper } from '@material-ui/core';
 import { noop } from 'lodash';
 import React, { useState } from 'react';
+import { IZComponentActionable } from '../component/component-actionable.interface';
+import { IZComponentConfirmable } from '../component/component-confirmable.interface';
+import { IZComponentDisabled } from '../component/component-disabled.interface';
+import { IZComponentHeader } from '../component/component-header.interface';
+import { IZComponentHierarchy } from '../component/component-hierarchy.interface';
+import { IZComponentLoading } from '../component/component-loading.interface';
+import { IZComponentMedia } from '../component/component-media.interface';
+import { IZComponentSizeable } from '../component/component-sizeable.interface';
+import { IZComponentStyle } from '../component/component-style.interface';
 import { ZCircularBackdrop } from '../loading/circular-backdrop';
-import { IZPaperCardProps } from './paper-card.props';
+
+/**
+ * Represents the properties for the paper card component.
+ */
+export interface IZPaperCardProps extends IZComponentHeader, IZComponentHierarchy, IZComponentStyle, IZComponentMedia, IZComponentSizeable, IZComponentLoading, IZComponentDisabled, IZComponentActionable, IZComponentConfirmable {}
 
 /**
  * Renders a material ui card wrapped in paper at a standard elevation.
@@ -12,7 +25,30 @@ import { IZPaperCardProps } from './paper-card.props';
  * @returns The jsx for the paper card.
  */
 export function ZPaperCard(props: IZPaperCardProps): JSX.Element {
-  const [confirmed, setConfirmed] = useState(props.autoConfirm);
+  const {
+    headerText,
+    subHeaderText = '',
+    className = '',
+    children = null,
+    loading = false,
+    disabled = false,
+    size = 'auto',
+    imageUrl = null,
+    imageWidth = 'auto',
+    imageHeight = 'auto',
+    actionText = null,
+    actionType = 'button',
+    actionColor = 'primary',
+    confirmation = null,
+    confirmationName = null,
+    confirmationColor = 'default',
+    autoConfirm = false,
+    avatar = null,
+
+    onAction = noop
+  } = props;
+
+  const [confirmed, setConfirmed] = useState(autoConfirm);
 
   /**
    * Sets the confirmed flag.
@@ -27,8 +63,8 @@ export function ZPaperCard(props: IZPaperCardProps): JSX.Element {
    * Occurs when the action button is clicked.
    */
   function handleAction() {
-    setConfirmed(props.autoConfirm);
-    props.onAction();
+    setConfirmed(autoConfirm);
+    onAction();
   }
 
   /**
@@ -37,7 +73,7 @@ export function ZPaperCard(props: IZPaperCardProps): JSX.Element {
    * @returns The jsx for the CardHeader component.
    */
   function createHeader() {
-    return <CardHeader className='ZPaperCard-header' avatar={props.avatar} title={<h3>{props.headerText}</h3>} subheader={props.subHeaderText} />;
+    return <CardHeader className='ZPaperCard-header' avatar={avatar} title={<h3>{headerText}</h3>} subheader={subHeaderText} />;
   }
 
   /**
@@ -46,9 +82,7 @@ export function ZPaperCard(props: IZPaperCardProps): JSX.Element {
    * @returns The jsx for the CardMedia component or null if the imageUrl is not set.
    */
   function createMedia() {
-    return props.imageUrl ? (
-      <CardMedia className={`ZPaperCard-media ZPaperCard-media-width-${props.imageWidth} ZPaperCard-media-height-${props.imageHeight}`} data-testid='ZPaperCard-media' component='img' image={props.imageUrl} title={props.headerText} />
-    ) : null;
+    return imageUrl ? <CardMedia className={`ZPaperCard-media ZPaperCard-media-width-${imageWidth} ZPaperCard-media-height-${imageHeight}`} data-testid='ZPaperCard-media' component='img' image={imageUrl} title={headerText} /> : null;
   }
 
   /**
@@ -58,12 +92,12 @@ export function ZPaperCard(props: IZPaperCardProps): JSX.Element {
    */
   function createContent() {
     const confirm =
-      props.confirmation && props.actionText ? (
-        <FormControlLabel className='ZPaperCard-actions-confirm' control={<Checkbox checked={confirmed} onChange={updateConfirmed} color={props.confirmationColor} name={props.confirmationName} />} label={props.confirmation} disabled={props.disabled} />
+      confirmation && actionText ? (
+        <FormControlLabel className='ZPaperCard-actions-confirm' control={<Checkbox checked={confirmed} onChange={updateConfirmed} color={confirmationColor} name={confirmationName} />} label={confirmation} disabled={disabled} />
       ) : null;
     return (
       <CardContent>
-        {props.children}
+        {children}
         {confirm}
       </CardContent>
     );
@@ -75,24 +109,24 @@ export function ZPaperCard(props: IZPaperCardProps): JSX.Element {
    * @returns The jsx for the card actions.
    */
   function createActions() {
-    if (!props.actionText) {
+    if (!actionText) {
       return null;
     }
 
-    const disabled = props.disabled || (props.confirmation && !confirmed);
+    const isDisabled = disabled || (confirmation && !confirmed);
 
     return (
       <CardActions className='ZPaperCard-actions' data-testid='ZPaperCard-actions'>
-        <Button className='ZPaperCard-btn-action' data-testid='ZPaperCard-btn-action' fullWidth={true} variant='outlined' type={props.actionType} disabled={disabled} color={props.actionColor} onClick={handleAction}>
-          {props.actionText}
+        <Button className='ZPaperCard-btn-action' data-testid='ZPaperCard-btn-action' fullWidth={true} variant='outlined' type={actionType} disabled={isDisabled} color={actionColor} onClick={handleAction}>
+          {actionText}
         </Button>
       </CardActions>
     );
   }
 
   return (
-    <Paper className={`${props.className} ZPaperCard-root ZPaperCard-size-${props.size}`} data-testid={props['data-testid']} elevation={5}>
-      <ZCircularBackdrop className='ZPaperCard-progress-loading' data-testid='ZPaperCard-progress-loading' show={props.loading} size='2.5em' />
+    <Paper className={`${className} ZPaperCard-root ZPaperCard-size-${size}`} data-testid={props['data-testid']} elevation={5}>
+      <ZCircularBackdrop className='ZPaperCard-progress-loading' data-testid='ZPaperCard-progress-loading' show={loading} size='2.5em' />
       <Card>
         {createHeader()}
         {createMedia()}
@@ -102,31 +136,3 @@ export function ZPaperCard(props: IZPaperCardProps): JSX.Element {
     </Paper>
   );
 }
-
-ZPaperCard.defaultProps = {
-  subHeaderText: '',
-
-  className: '',
-  children: null,
-
-  loading: false,
-  disabled: false,
-  size: 'auto',
-
-  imageUrl: null,
-  imageWidth: 'auto',
-  imageHeight: 'auto',
-
-  actionText: null,
-  actionType: 'button',
-  actionColor: 'primary',
-  onAction: noop,
-
-  confirmation: null,
-  confirmationName: null,
-  confirmationColor: 'default',
-  autoConfirm: false,
-
-  avatar: null,
-  action: null
-};
