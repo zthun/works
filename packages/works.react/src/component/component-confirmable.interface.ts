a78cdaf9c5a0b868d1efb8fa12abb9b0459053fe
Anthony Bonta <zthun.coding@gmail.com>
2021-09-02 21:14:12 -0500

    feat: the properties on the IZComponentConfirmable are now optional

--- packages/works.react/src/component/component-confirmable.interface.ts
+++ packages/works.react/src/component/component-confirmable.interface.ts
@@ -7,20 +7,20 @@ export interface IZComponentConfirmable {
   /**
    * The confirmation display.
    */
-  confirmation: ReactNode;
+  confirmation?: ReactNode;
 
   /**
    * The confirmation color.
    */
-  confirmationColor: 'primary' | 'default' | 'secondary';
+  confirmationColor?: 'primary' | 'default' | 'secondary';
 
   /**
    * The form name of the confirmation.  Mostly only used with forms.
    */
-  confirmationName: string;
+  confirmationName?: string;
 
   /**
    * Sets whether or not the confirmation is checked by default.
    */
-  autoConfirm: boolean;
+  autoConfirm?: boolean;
 }
