65deb0c24b46024193bdd2af28ef2446cbdf26ad
Anthony Bonta <zthun.coding@gmail.com>
2021-10-20 18:53:10 -0500

    refactor: merged the props files into the component file

--- packages/works.react/src/codes/status-code-page.props.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Represents properties for the status code page.
- */
-export interface IZStatusCodePageProps {
-  /**
-   * The code to display information about.
-   */
-  code: number | string;
-}
