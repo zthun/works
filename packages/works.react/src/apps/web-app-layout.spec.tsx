8d11c3b6e8e4086970aa171a5925e1723618c4e8
Anthony Bonta <zthun.coding@gmail.com>
2021-10-23 17:34:15 -0500

    test: fixed an issue with an open handle

--- packages/works.react/src/apps/web-app-layout.spec.tsx
+++ packages/works.react/src/apps/web-app-layout.spec.tsx
@@ -1,18 +1,38 @@
-import { act, render, RenderResult } from '@testing-library/react';
+import { render, waitFor } from '@testing-library/react';
+import { createMocked } from '@zthun/works.jest';
+import { ZUrlBuilder } from '@zthun/works.url';
 import React from 'react';
+import { IZProfileService, ZProfileServiceContext } from '../profile/profile-service.context';
 import { ZWebAppLayout } from './web-app-layout';
+import { IZWebAppService, ZWebAppServiceContext } from './web-app-service.context';
 
 /* eslint-disable require-jsdoc */
 describe('ZWebAppLayout', () => {
+  let profileService: jest.Mocked<IZProfileService>;
+  let webAppService: jest.Mocked<IZWebAppService>;
+
   async function createTestTarget() {
-    let target: RenderResult;
+    const target = render(
+      <ZProfileServiceContext.Provider value={profileService}>
+        <ZWebAppServiceContext.Provider value={webAppService}>
+          <ZWebAppLayout headerText='Test Webapp' />
+          );
+        </ZWebAppServiceContext.Provider>
+      </ZProfileServiceContext.Provider>
+    );
 
-    await act(async () => {
-      target = render(<ZWebAppLayout headerText='Test Webapp' />);
-    });
-
+    await waitFor(() => expect(target.container.querySelector('.ZWebAppLayout-root')).not.toBeNull());
     return target;
   }
+
+  beforeEach(() => {
+    profileService = createMocked<IZProfileService>(['read', 'getAvatar', 'getDisplay']);
+    profileService.read.mockRejectedValue('Not logged in.');
+    profileService.getAvatar.mockResolvedValue(new ZUrlBuilder().gravatar().build());
+    profileService.getDisplay.mockResolvedValue('');
+    webAppService = createMocked<IZWebAppService>(['list']);
+    webAppService.list.mockResolvedValue([]);
+  });
 
   it('should render the layout.', async () => {
     // Arrange
