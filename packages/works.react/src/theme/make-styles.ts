84762edb857c3d1199a5271305396a667a9703be
Anthony Bonta <zthun.coding@gmail.com>
2021-10-24 00:09:41 -0500

    docs: added docs for interfaces

--- packages/works.react/src/theme/make-styles.ts
+++ packages/works.react/src/theme/make-styles.ts
@@ -1,23 +1,79 @@
 import { createMakeStyles } from 'tss-react';
 import { Theme, useTheme as useMuiTheme } from '@mui/material';
 
+/**
+ * Represents the options for sizes for components.
+ */
 export interface ZthunworksSizeOptions {
+  /**
+   * Smallest possible size.
+   */
   xs?: number | string;
+  /**
+   * Small T-Shirt.
+   */
   sm?: number | string;
+  /**
+   * Medium T-Shirt.
+   *
+   * This should be the default in most cases.
+   */
   md?: number | string;
+  /**
+   * Large T-Shirt.
+   */
   lg?: number | string;
+  /**
+   * Extra large T-Shirt.
+   */
   xl?: number | string;
+  /**
+   * Maximum size.
+   *
+   * This will usually be 100%.
+   */
   max?: number | string;
+  /**
+   * Empty.
+   *
+   * Mostly a helper as this will usually be 0.
+   */
   none?: number | string;
 }
 
+/**
+ * Options for border rounding geometry.
+ */
 export interface ZthunworksGeometryOptions {
+  /**
+   * Square rounding.
+   *
+   * Basically hard edges.
+   */
   square?: number | string;
+
+  /**
+   * Circle rounding.
+   *
+   * Basically 50%.
+   */
   circle?: number | string;
 }
 
+/**
+ * The overall theme for the Zthunworks domain.
+ *
+ * This extends material UI's main theme and adds
+ * some helpers and size options.
+ */
 export interface ZthunworksTheme extends Theme {
+  /**
+   * Rounding options.
+   */
   rounding: ZthunworksGeometryOptions;
+  /**
+   * Sizing options.
+   */
   sizing: {
     alerts: ZthunworksSizeOptions;
     avatar: ZthunworksSizeOptions;
@@ -33,6 +89,8 @@ export interface ZthunworksTheme extends Theme {
 
 /**
  * Constructs the theme variables for zthunworks components.
+ *
+ * See https://www.npmjs.com/package/tss-react for more information.
  *
  * @returns The zthunworks theme.
  */
