7a0da36e96a93455023b8a7f45bf699f97f84043
Anthony Bonta <zthun.coding@gmail.com>
2021-10-24 00:22:36 -0500

    feat: converted circular backdrop to use css in js

--- packages/works.react/src/loading/circular-backdrop.tsx
+++ packages/works.react/src/loading/circular-backdrop.tsx
@@ -1,5 +1,29 @@
 import React from 'react';
+import { makeStyles } from '../theme/make-styles';
 import { IZCircularProgressProps, ZCircularProgress } from './circular-progress';
+
+const useCircularBackdropStyles = makeStyles()((theme) => ({
+  root: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    top: 0,
+    bottom: 0,
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+
+  backdrop: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    top: 0,
+    bottom: 0,
+    backgroundColor: theme.palette.common.black,
+    opacity: 0.25
+  }
+}));
 
 /**
  * Creates a circular progress spinner that is positioned absolutely and contains a backdrop to disable everything behind it.
@@ -10,10 +34,11 @@ import { IZCircularProgressProps, ZCircularProgress } from './circular-progress'
  */
 export function ZCircularBackdrop(props: IZCircularProgressProps) {
   const { show = true } = props;
+  const styles = useCircularBackdropStyles();
 
   return show ? (
-    <div className='ZCircularBackdrop-root'>
-      <div className='ZCircularBackdrop-backdrop' />
+    <div className={`ZCircularBackdrop-root ${styles.classes.root}`}>
+      <div className={`ZCircularBackdrop-backdrop ${styles.classes.backdrop}`} />
       <ZCircularProgress {...props} />
     </div>
   ) : null;
