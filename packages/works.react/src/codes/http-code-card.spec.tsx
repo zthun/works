37fb522ceaf252de196d45b53a64cadd91d85fcb
Anthony Bonta <zthun.coding@gmail.com>
2021-10-23 23:58:41 -0500

    feat: added support for code context display
    1. Converted to use css in js
    2. The icon will now change depending on the code.
    3. The header will now display the code category.
    4. The color of the icon will change depending on the code.

--- packages/works.react/src/codes/http-code-card.spec.tsx
+++ packages/works.react/src/codes/http-code-card.spec.tsx
@@ -1,12 +1,12 @@
 /* eslint-disable require-jsdoc */
 
 import { render } from '@testing-library/react';
-import { ZHttpCodeClient } from '@zthun/works.http';
+import { ZHttpCode, ZHttpCodeClient, ZHttpCodeInformationalResponse, ZHttpCodeRedirection, ZHttpCodeServer, ZHttpCodeSuccess } from '@zthun/works.http';
 import React from 'react';
 import { ZHttpStatusCodeCard } from './http-code-card';
 
 describe('ZHttpStatusCodeCard', () => {
-  let code: number;
+  let code: ZHttpCode;
 
   function createTestTarget() {
     return render(<ZHttpStatusCodeCard code={code} />);
@@ -16,22 +16,48 @@ describe('ZHttpStatusCodeCard', () => {
     code = ZHttpCodeClient.NotFound;
   });
 
-  it('should render the component.', () => {
-    // Arrange
-    const target = createTestTarget();
-    // Act
-    const actual = target.getByTestId('ZHttpStatusCodeCard-root');
-    // Assert
-    expect(actual).toBeTruthy();
-  });
+  describe('Icons', () => {
+    function assertIcon(expected: string, c: ZHttpCode) {
+      // Arrange
+      code = c;
+      const target = createTestTarget();
+      // Act
+      const icon = target.getByTestId(`ZHttpStatusCodeCard-${expected}`);
+      // Assert
+      expect(icon).toBeTruthy();
+    }
+    describe('Informational Response', () => {
+      it('should render an info icon.', () => {
+        assertIcon('info', ZHttpCodeInformationalResponse.Continue);
+      });
+    });
 
-  it('should be short and stout.', () => {
-    // Arrange
-    code = ZHttpCodeClient.ImATeapot;
-    const target = createTestTarget();
-    // Act
-    const actual = target.getByTestId('ZHttpStatusCodeCard-teapot');
-    // Assert
-    expect(actual).toBeTruthy();
+    describe('Success', () => {
+      it('should render a success icon.', () => {
+        assertIcon('success', ZHttpCodeSuccess.OK);
+      });
+    });
+
+    describe('Redirection', () => {
+      it('should render a redirect icon.', () => {
+        assertIcon('redirect', ZHttpCodeRedirection.MultipleChoices);
+      });
+    });
+
+    describe('Client', () => {
+      it('should render a client icon.', () => {
+        assertIcon('client', ZHttpCodeClient.BadRequest);
+      });
+
+      it('should render a teapot icon.', () => {
+        assertIcon('teapot', ZHttpCodeClient.ImATeapot);
+      });
+    });
+
+    describe('Server', () => {
+      it('should render a server icon.', () => {
+        assertIcon('server', ZHttpCodeServer.InternalServerError);
+      });
+    });
   });
 });
--- packages/works.react/src/codes/http-code-card.tsx
+++ packages/works.react/src/codes/http-code-card.tsx
@@ -1,9 +1,14 @@
 import { Typography } from '@mui/material';
 import EmojiFoodBeverageTwoToneIcon from '@mui/icons-material/EmojiFoodBeverageTwoTone';
 import WarningIcon from '@mui/icons-material/Warning';
-import { getHttpCodeDescription, getHttpCodeName, ZHttpCode, ZHttpCodeClient } from '@zthun/works.http';
+import InfoIcon from '@mui/icons-material/Info';
+import ForwardIcon from '@mui/icons-material/Forward';
+import CheckCircleIcon from '@mui/icons-material/CheckCircle';
+import ErrorIcon from '@mui/icons-material/Error';
+import { getHttpCodeCategory, getHttpCodeDescription, getHttpCodeName, getHttpCodeSeverity, ZHttpCode, ZHttpCodeClient, ZHttpCodeSeverity } from '@zthun/works.http';
 import React from 'react';
 import { ZPaperCard } from '../card/paper-card';
+import { makeStyles } from '../theme/make-styles';
 
 /**
  * Represents a status summary card for an http error code.
@@ -15,6 +20,36 @@ export interface IZHttpStatusCodeCardProps {
   code: ZHttpCode;
 }
 
+const useHttpStatusCodeCardStyles = makeStyles<IZHttpStatusCodeCardProps>()((theme, props) => {
+  const { code } = props;
+
+  const colors = {
+    [ZHttpCodeSeverity.Error]: theme.palette.error.main,
+    [ZHttpCodeSeverity.Warning]: theme.palette.warning.main,
+    [ZHttpCodeSeverity.Info]: theme.palette.info.main,
+    [ZHttpCodeSeverity.Success]: theme.palette.success.main
+  };
+
+  const severity = getHttpCodeSeverity(code);
+
+  return {
+    description: {
+      fontSize: theme.sizing.font.lg,
+      maxWidth: '30rem'
+    },
+    code: {
+      fontSize: '6rem',
+      color: theme.palette.secondary.main,
+      opacity: '0.75',
+      textAlign: 'center'
+    },
+    icon: {
+      fontSize: '4rem',
+      color: colors[severity]
+    }
+  };
+});
+
 /**
  * Renders a paper card that describes an HttpStatusCode.
  *
@@ -25,20 +60,53 @@ export interface IZHttpStatusCodeCardProps {
 export function ZHttpStatusCodeCard(props: IZHttpStatusCodeCardProps) {
   const { code } = props;
   const name = getHttpCodeName(code);
-  const heading = 'Error';
+  const heading = getHttpCodeCategory(code);
   const description = getHttpCodeDescription(code);
-  let avatar = <WarningIcon className='ZHttpStatusCodeCard-icon ZHttpStatusCodeCard-client' data-testid='ZHttpStatusCodeCard-client' />;
+  const styles = useHttpStatusCodeCardStyles(props);
 
-  if (props.code === ZHttpCodeClient.ImATeapot) {
-    avatar = <EmojiFoodBeverageTwoToneIcon className='ZHttpStatusCodeCard-icon ZHttpStatusCodeCard-teapot' data-testid='ZHttpStatusCodeCard-teapot' />;
+  /**
+   * Renders the avatar based on the category of the code being presented.
+   *
+   * @returns The avatar of the code being presented.
+   */
+  function renderAvatar() {
+    if (code === ZHttpCodeClient.ImATeapot) {
+      return <EmojiFoodBeverageTwoToneIcon className={`ZHttpStatusCodeCard-icon ZHttpStatusCodeCard-teapot ${styles.classes.icon}`} data-testid='ZHttpStatusCodeCard-teapot' />;
+    }
+
+    if (code >= 100 && code < 200) {
+      return <InfoIcon className={`ZHttpStatusCodeCard-icon ZHttpStatusCodeCard-info ${styles.classes.icon}`} data-testid='ZHttpStatusCodeCard-info' />;
+    }
+
+    if (code >= 200 && code < 300) {
+      return <CheckCircleIcon className={`ZHttpStatusCodeCard-icon ZHttpStatusCodeCard-success ${styles.classes.icon}`} data-testid='ZHttpStatusCodeCard-success' />;
+    }
+
+    if (code >= 300 && code < 400) {
+      return <ForwardIcon className={`ZHttpStatusCodeCard-icon ZHttpStatusCodeCard-redirect ${styles.classes.icon}`} data-testid='ZHttpStatusCodeCard-redirect' />;
+    }
+
+    if (code >= 400 && code < 500) {
+      return <WarningIcon className={`ZHttpStatusCodeCard-icon ZHttpStatusCodeCard-client ${styles.classes.icon}`} data-testid='ZHttpStatusCodeCard-client' />;
+    }
+
+    return <ErrorIcon className={`ZHttpStatusCodeCard-icon ZHttpStatusCodeCard-server ${styles.classes.icon}`} data-testid='ZHttpStatusCodeCard-server' />;
   }
 
+  /*
+  let avatar = <WarningIcon className={`ZHttpStatusCodeCard-icon ZHttpStatusCodeCard-client ${styles.classes.icon}`} data-testid='ZHttpStatusCodeCard-client' />;
+
+  if (props.code === ZHttpCodeClient.ImATeapot) {
+    avatar = <EmojiFoodBeverageTwoToneIcon className={`ZHttpStatusCodeCard-icon ZHttpStatusCodeCard-teapot ${styles.classes.icon}`} data-testid='ZHttpStatusCodeCard-teapot' />;
+  }
+  */
+
   return (
-    <ZPaperCard className='ZHttpStatusCodeCard-root' data-testid='ZHttpStatusCodeCard-root' avatar={avatar} headerText={heading} subHeaderText={name}>
-      <Typography variant='body1' component='p' className='ZHttpStatusCodeCard-description'>
+    <ZPaperCard className='ZHttpStatusCodeCard-root' data-testid='ZHttpStatusCodeCard-root' avatar={renderAvatar()} headerText={heading} subHeaderText={name}>
+      <Typography variant='body1' component='p' className={`ZHttpStatusCodeCard-description ${styles.classes.description}`}>
         {description}
       </Typography>
-      <Typography variant='body2' component='div' className={`ZHttpStatusCodeCard-code ZHttpStatusCodeCard-code-${code}`}>
+      <Typography variant='body2' component='div' className={`ZHttpStatusCodeCard-code ZHttpStatusCodeCard-code-${code} ${styles.classes.code}`}>
         {props.code}
       </Typography>
     </ZPaperCard>
