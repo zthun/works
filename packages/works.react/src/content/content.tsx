b175257e118832c640725b869748544082f75df3
Anthony Bonta <zthun.coding@gmail.com>
2021-10-24 00:14:36 -0500

    style: the content component now uses css in js

--- packages/works.react/src/content/content.tsx
+++ packages/works.react/src/content/content.tsx
@@ -1,5 +1,12 @@
 import React from 'react';
+import { makeStyles } from '../theme/make-styles';
 import { IZComponentHierarchy } from '../component/component-hierarchy.interface';
+
+const useContentStyles = makeStyles()((theme) => ({
+  root: {
+    padding: theme.sizing.gaps.xl
+  }
+}));
 
 /**
  * Returns the jsx for root content.
@@ -9,5 +16,7 @@ import { IZComponentHierarchy } from '../component/component-hierarchy.interface
  * @returns The jsx for root content.
  */
 export function ZContent(props: IZComponentHierarchy) {
-  return <article className='ZContent-root'>{props.children}</article>;
+  const styles = useContentStyles();
+
+  return <article className={`ZContent-root ${styles.classes.root}`}>{props.children}</article>;
 }
