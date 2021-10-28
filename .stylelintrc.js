1621816c6fff241fe673d5ca9e2988a5ed51cb5e
Anthony Bonta <zthun.coding@gmail.com>
2021-10-27 22:31:27 -0500

    chore: remove stylelint

--- .stylelintrc.js
+++
@@ -1,7 +0,0 @@
-const config = require('@zthun/stylelint-config');
-
-config.rules = config.rules || {};
-config.rules['no-descending-specificity'] = null;
-config.rules['declaration-empty-line-before'] = 'never';
-
-module.exports = config;
