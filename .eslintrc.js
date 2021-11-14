ae8856579ae2c93a67d82fe365ed1f5ddb50c299
Anthony Bonta <zthun.coding@gmail.com>
2021-11-14 15:24:06 -0600

    chore: updating eslint file to easier readability

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,20 @@
-const config = require('@zthun/eslint-config');
-
-config.settings = config.settings || {};
-config.settings.react = config.settings.react || {};
-config.settings.react.version = 'detect';
-
-config.rules = config.rules || {};
-config.rules['no-use-before-define'] = 'off';
-config.rules['react/prop-types'] = 'off';
-config.rules['quotes'] = ['error', 'single', { avoidEscape: true, allowTemplateLiterals: true }];
-
-module.exports = config;
+module.exports = {
+  extends: ['@zthun/eslint-config'],
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  },
+  rules: {
+    'no-use-before-define': 'off',
+    'quotes': [
+      'error',
+      'single',
+      {
+        avoidEscape: true,
+        allowTemplateLiterals: true
+      }
+    ],
+    'react/prop-types': 'off'
+  }
+};
