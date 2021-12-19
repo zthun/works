a84384842eda4c0395ea7d8b1b1247ac58f20c60
Anthony Bonta <zthun.coding@gmail.com>
2021-12-18 19:50:28 -0600

    feat: added support for an assertion that a sendable proxy sends a pattern and payload

--- packages/works.jest/src/index.ts
+++ packages/works.jest/src/index.ts
@@ -1,4 +1,5 @@
 /* istanbul ignore file */
 
 export { assertBuilderAssignsObject, assertBuilderCopiesObject, assertBuilderSetsProperty } from './builder/assertBuilder.function';
+export { assertProxySendsMessage } from './proxy/assertProxy';
 export { createMocked } from './spy/create-mocked.function';
