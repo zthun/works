7a8ab928162710f458fda16250afd14cc4392553
Anthony Bonta <zthun.coding@gmail.com>
2021-12-21 17:56:53 -0600

    test: update to identity service

--- packages/works.react/src/apps/web-app-layout.spec.tsx
+++ packages/works.react/src/apps/web-app-layout.spec.tsx
@@ -2,23 +2,23 @@ import { render, waitFor } from '@testing-library/react';
 import { createMocked } from '@zthun/works.jest';
 import { ZUrlBuilder } from '@zthun/works.url';
 import React from 'react';
-import { IZProfileService, ZProfileServiceContext } from '../profile/profile-service.context';
+import { IZIdentityService, ZIdentityServiceContext } from '../identity/identity-service.context';
 import { ZWebAppLayout } from './web-app-layout';
 import { IZWebAppService, ZWebAppServiceContext } from './web-app-service.context';
 
 /* eslint-disable require-jsdoc */
 describe('ZWebAppLayout', () => {
-  let profileService: jest.Mocked<IZProfileService>;
+  let profileService: jest.Mocked<IZIdentityService>;
   let webAppService: jest.Mocked<IZWebAppService>;
 
   async function createTestTarget() {
     const target = render(
-      <ZProfileServiceContext.Provider value={profileService}>
+      <ZIdentityServiceContext.Provider value={profileService}>
         <ZWebAppServiceContext.Provider value={webAppService}>
           <ZWebAppLayout headerText='Test Webapp' />
           );
         </ZWebAppServiceContext.Provider>
-      </ZProfileServiceContext.Provider>
+      </ZIdentityServiceContext.Provider>
     );
 
     await waitFor(() => expect(target.container.querySelector('.ZWebAppLayout-root')).not.toBeNull());
@@ -26,7 +26,7 @@ describe('ZWebAppLayout', () => {
   }
 
   beforeEach(() => {
-    profileService = createMocked<IZProfileService>(['read', 'getAvatar', 'getDisplay']);
+    profileService = createMocked(['read', 'getAvatar', 'getDisplay']);
     profileService.read.mockRejectedValue('Not logged in.');
     profileService.getAvatar.mockResolvedValue(new ZUrlBuilder().gravatar().build());
     profileService.getDisplay.mockResolvedValue('');
