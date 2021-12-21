37b3d66fee9e42722616f708a7f784af0692b5f9
Anthony Bonta <zthun.coding@gmail.com>
2021-12-21 14:20:33 -0600

    feat!: removed the profile and login from react
    These were moved over to roadblock

--- packages/works.server/src/main.ts
+++ packages/works.server/src/main.ts
@@ -1,12 +1,3 @@
-import { Module } from '@nestjs/common';
-import { ZAuthModule, ZNestApplication, ZNestApplicationModule } from '@zthun/works.nest';
+import { ZNestApplication, ZNestApplicationModule } from '@zthun/works.nest';
 
-@Module({
-  imports: [ZAuthModule, ZNestApplicationModule]
-})
-/**
- * The main module.
- */
-export class ZMainModule {}
-
-ZNestApplication.create(ZMainModule).then((app) => ZNestApplication.run(app));
+ZNestApplication.create(ZNestApplicationModule).then((app) => ZNestApplication.run(app));
