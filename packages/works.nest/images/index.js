6ee2df80469f48a2fbbab8e7930249f4a169b16b
Anthony Bonta <zthun.coding@gmail.com>
2021-12-21 12:25:03 -0600

    feat!: removed images no longer relevant to works.nest

--- packages/works.nest/images/index.js
+++ packages/works.nest/images/index.js
@@ -1,9 +1,5 @@
 // PNG
-require('./png/works.nest.auth.png');
 require('./png/works.nest.health.png');
-require('./png/works.nest.notifications.png');
-require('./png/works.nest.users.png');
-require('./png/works.nest.vault.png');
 
 // SVG
 require('./svg/works.nest.svg');
