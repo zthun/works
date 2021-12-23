9827e5e0b6d6858bc38ae31dbfcca482b1182d17
Anthony Bonta <zthun.coding@gmail.com>
2021-12-23 00:19:13 -0600

    fix: the works.api now properly adds in the standard controllers

--- packages/works.api/src/main.ts
+++ packages/works.api/src/main.ts
@@ -1,12 +1,3 @@
-import { Module } from '@nestjs/common';
-import { ZHealthModule, ZNestApplication } from '@zthun/works.nest';
+import { ZNestApplication, ZNestApplicationModule } from '@zthun/works.nest';
 
-@Module({
-  imports: [ZHealthModule]
-})
-/**
- * The main module.
- */
-export class ZMainModule {}
-
-ZNestApplication.create(ZMainModule).then((app) => ZNestApplication.run(app));
+ZNestApplication.create(ZNestApplicationModule).then((app) => ZNestApplication.run(app));
