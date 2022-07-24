ba51284187ca2c4cffeac3dfe8f30d9c7e30b642
Anthony Bonta <zthun.coding@gmail.com>
2022-07-24 17:10:44 -0500

    refactor: file paths

--- packages/works.http/src/index.ts
+++ packages/works.http/src/index.ts
@@ -1,19 +1,15 @@
 /* istanbul ignore file */
 
 // Request
-export { ZHttpMethod } from './request/http-method.enum';
-export { ZHttpRequestBuilder } from './request/http-request-builder.class';
-export { IZHttpRequest } from './request/http-request.interface';
+export { IZHttpRequest, ZHttpMethod, ZHttpRequestBuilder } from './request/http-request';
 // Result
-export { ZHttpCodeClient, ZHttpCodeClientDescriptions, ZHttpCodeClientNames } from './result/http-code-client.enum';
-export { ZHttpCodeInformationalResponse, ZHttpCodeInformationalResponseNames, ZHttpCodeInformationalResponseDescriptions } from './result/http-code-informational-response.enum';
-export { ZHttpCodeRedirection, ZHttpCodeRedirectionNames, ZHttpCodeRedirectionDescriptions } from './result/http-code-redirection.enum';
-export { ZHttpCodeServer, ZHttpCodeServerNames, ZHttpCodeServerDescriptions } from './result/http-code-server.enum';
-export { ZHttpCodeSuccess, ZHttpCodeSuccessNames, ZHttpCodeSuccessDescriptions } from './result/http-code-success.enum';
-export { ZHttpCode, ZHttpCodeSeverity, ZHttpCodeCategory, getHttpCodeName, getHttpCodeDescription, getHttpCodeSeverity, getHttpCodeCategory } from './result/http-code.type';
-export { ZHttpResultBuilder } from './result/http-result-builder.class';
-export { IZHttpResult } from './result/http-result.interface';
+export { getHttpCodeCategory, getHttpCodeDescription, getHttpCodeName, getHttpCodeSeverity, ZHttpCode, ZHttpCodeCategory, ZHttpCodeSeverity } from './result/http-code';
+export { ZHttpCodeClient, ZHttpCodeClientDescriptions, ZHttpCodeClientNames } from './result/http-code-client';
+export { ZHttpCodeInformationalResponse, ZHttpCodeInformationalResponseDescriptions, ZHttpCodeInformationalResponseNames } from './result/http-code-informational-response';
+export { ZHttpCodeRedirection, ZHttpCodeRedirectionDescriptions, ZHttpCodeRedirectionNames } from './result/http-code-redirection';
+export { ZHttpCodeServer, ZHttpCodeServerDescriptions, ZHttpCodeServerNames } from './result/http-code-server';
+export { ZHttpCodeSuccess, ZHttpCodeSuccessDescriptions, ZHttpCodeSuccessNames } from './result/http-code-success';
+export { IZHttpResult, ZHttpResultBuilder } from './result/http-result';
 // Service
-export { ZHttpService } from './service/http-service.class';
-export { ZHttpServiceMock } from './service/http-service-mock.class';
-export { IZHttpService } from './service/http-service.interface';
+export { IZHttpService, ZHttpService } from './service/http-service';
+export { ZHttpServiceMock } from './service/http-service-mock';
