13bc74178838a67f54abe0bc7972cba842b7c17f
Anthony Bonta <zthun.coding@gmail.com>
2022-07-30 14:32:07 -0500

    chore: navigate to the button page

--- packages/works.web/src/web-apps/web-apps-page.tsx
+++ packages/works.web/src/web-apps/web-apps-page.tsx
@@ -1,12 +1,13 @@
 import ElectricalServicesIcon from '@mui/icons-material/ElectricalServices';
 import MedicalServicesIcon from '@mui/icons-material/MedicalServices';
 import SettingsIcon from '@mui/icons-material/Settings';
+import SmartButtonIcon from '@mui/icons-material/SmartButton';
 import StartIcon from '@mui/icons-material/Start';
 import WarningIcon from '@mui/icons-material/Warning';
-import { makeStyles, useNavigate, ZMenu, ZMenuItem, ZNavigate, ZRoute, ZRouteMap, ZToolbar } from '@zthun/works.react';
+import { makeStyles, useNavigate, ZButton, ZMenu, ZMenuItem, ZNavigate, ZRoute, ZRouteMap, ZToolbar } from '@zthun/works.react';
 import React from 'react';
-import { ZButton } from '../../../works.react/src/buttons/button';
 import { ZAlertsPage } from './components/alerts/alerts-page';
+import { ZButtonPage } from './components/button/button-page';
 import { ZGettingStartedPage } from './getting-started/getting-started-page';
 
 const useWebAppPageStyles = makeStyles()((theme) => ({
@@ -34,6 +35,9 @@ export function ZWebAppsPage() {
           <ZMenuItem avatar={<WarningIcon color='warning' />} onClick={navigate.bind(null, 'components/alerts')}>
             Alerts
           </ZMenuItem>
+          <ZMenuItem avatar={<SmartButtonIcon color='error' />} onClick={navigate.bind(null, 'components/button')}>
+            Button
+          </ZMenuItem>
         </ZMenu>
         <ZMenu avatar={<ElectricalServicesIcon color='warning' />} color='info' outline headerText='Services'>
           <ZMenuItem avatar={<MedicalServicesIcon color='success' />}>Health</ZMenuItem>
@@ -44,6 +48,7 @@ export function ZWebAppsPage() {
         <ZRoute path='getting-started' element={<ZGettingStartedPage />} />
         <ZRoute path='components'>
           <ZRoute path='alerts' element={<ZAlertsPage />} />
+          <ZRoute path='button' element={<ZButtonPage />} />
         </ZRoute>
         <ZRoute path='/' element={<ZNavigate to='getting-started' />} />
         <ZRoute path='*' element={<ZNavigate to={'/status-code/404'} />} />
