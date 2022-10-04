1f251e0f310469e74cacbfa55bf5214c0e8f86da
Anthony Bonta <zthun.coding@gmail.com>
2022-10-03 21:32:17 -0500

    refactor: abstracted out the choice drop down to an common set of functionality

--- packages/works.react/src/form/choice-drop-down.cm.ts
+++ packages/works.react/src/form/choice-drop-down.cm.ts
@@ -1,34 +1,23 @@
 import { IZCircusPerformer, IZCircusWait, required, ZCircusActBuilder } from '@zthun/works.cirque';
-import { find } from 'lodash';
 import { ZChoiceOptionComponentModel } from './choice-option.cm';
+import { ZChoiceComponentModel } from './choice.cm';
 
 /**
  * Represents a React Component Model that describes a ZChoice component.
  */
-export class ZChoiceDropDownComponentModel {
+export class ZChoiceDropDownComponentModel extends ZChoiceComponentModel {
   /**
    * Initializes a new instance of this object.
    *
-   * @param _element
+   * @param element
    *        The element that represents the root of the ZChoice object.
-   * @param _performer
+   * @param performer
    *        The circus performer responsible for performing user actions.
-   * @param _wait
+   * @param wait
    *        The wait implementation to halt the circus.
    */
-  public constructor(
-    private readonly _element: HTMLElement,
-    private readonly _performer: IZCircusPerformer,
-    private readonly _wait: IZCircusWait
-  ) {}
-
-  /**
-   * Gets the list of selected items.
-   */
-  public get selected(): ZChoiceOptionComponentModel[] {
-    return Array.from(this._element.querySelectorAll<HTMLElement>('.ZChoice-value')).map(
-      (e: HTMLElement) => new ZChoiceOptionComponentModel(e)
-    );
+  public constructor(element: HTMLElement, performer: IZCircusPerformer, wait: IZCircusWait) {
+    super(element, performer, wait);
   }
 
   /**
@@ -81,24 +70,6 @@ export class ZChoiceDropDownComponentModel {
 
     const act = new ZCircusActBuilder().moveTo(cross).leftMouseClick().build();
     await this._performer.perform(act);
-  }
-
-  /**
-   * Performs a selection on a specific option.
-   *
-   * @param option The option to select.
-   */
-  public async select(option: ZChoiceOptionComponentModel | string | number): Promise<void> {
-    const options = await this.open();
-    const value = option instanceof ZChoiceOptionComponentModel ? option.value : String(option);
-    const optionToSelect = find(options, (op) => op.value === value);
-
-    if (optionToSelect != null) {
-      const act = new ZCircusActBuilder().moveTo(optionToSelect.element).leftMouseClick().build();
-      await this._performer.perform(act);
-    }
-
-    await this.close();
   }
 
   /**
