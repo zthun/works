fb5a80471fcea5b60ec6cb9bf13f999761f40b60
Anthony Bonta <zthun.coding@gmail.com>
2022-10-12 11:44:27 -0500

    feat: you can now assign the severity by variable instead of being forced to invoke a member method

--- packages/works.web/src/web-apps/components/alerts/alerts-page.tsx
+++ packages/works.web/src/web-apps/components/alerts/alerts-page.tsx
@@ -1,3 +1,8 @@
+import CheckCircleIcon from '@mui/icons-material/CheckCircle';
+import WarningIcon from '@mui/icons-material/Warning';
+import { ZAlertBuilder } from '@zthun/works.message';
+import { ZButton, ZGridLayout, ZPaperCard, ZStateColor } from '@zthun/works.react';
+
 import React from 'react';
 
 /**
@@ -6,5 +11,32 @@ import React from 'react';
  * @returns The JSX to render the alerts demo page.
  */
 export function ZAlertsPage() {
-  return <div className='ZAlertsPage-root'>Alerts!</div>;
+  /**
+   * Opens a specific alert.
+   *
+   * @param color
+   *        The color of the alert to open.
+   */
+  async function openAlert(color: ZStateColor): Promise<void> {
+    const alert = new ZAlertBuilder().
+  }
+
+  return (
+    <ZPaperCard
+      className='ZButtonPage-root'
+      headerText='Alerts'
+      subHeaderText='User feedback'
+      avatar={<WarningIcon color='warning' fontSize='large' />}
+    >
+      <ZGridLayout columns='auto auto' gap='md'>
+        <ZButton
+          avatar={<CheckCircleIcon fontSize='small' />}
+          color={ZStateColor.Success}
+          outline
+          onClick={openAlert.bind(null, ZStateColor.Success)}
+          label='Success'
+        />
+      </ZGridLayout>
+    </ZPaperCard>
+  );
 }
