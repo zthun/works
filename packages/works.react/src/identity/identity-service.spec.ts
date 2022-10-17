f6b24e6d8b2931ab48f72c559899efd42d11c064
Anthony Bonta <zthun.coding@gmail.com>
2022-10-16 23:49:18 -0500

    chore: cleanup

--- packages/works.react/src/identity/identity-service.spec.ts
+++ packages/works.react/src/identity/identity-service.spec.ts
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/no-non-null-assertion */
 /* eslint-disable require-jsdoc */
-import { IZProfile, ZProfileAvatarSize, ZProfileBuilder } from '@zthun/works.core';
+import { IZProfile, ZProfileBuilder } from '@zthun/works.core';
 import {
   ZHttpCodeServer,
   ZHttpCodeSuccess,
@@ -8,9 +8,7 @@ import {
   ZHttpResultBuilder,
   ZHttpServiceMock
 } from '@zthun/works.http';
-import { ZUrlBuilder } from '@zthun/works.url';
-import md5 from 'md5';
-import { ZIdentityService } from './identity-service.context';
+import { ZIdentityService } from './identity-service';
 
 describe('ZIdentityService', () => {
   let http: ZHttpServiceMock;
@@ -67,79 +65,6 @@ describe('ZIdentityService', () => {
       const actual = await target.read();
       // Assert
       expect(actual).toBeNull();
-    });
-  });
-
-  describe('Avatar', () => {
-    it('should return the default gravatar url for a falsy profile.', async () => {
-      // Arrange
-      const expected = new ZUrlBuilder().gravatar(undefined, ZProfileAvatarSize).build();
-      const target = createTestTarget();
-      // Act
-      const actual = await target.getAvatar(null);
-      // Assert
-      expect(actual).toEqual(expected);
-    });
-
-    it('should return the gravatar url if the avatar for the profile is not set.', async () => {
-      // Arrange
-      const hash = md5(profile.email!);
-      profile = new ZProfileBuilder().copy(profile).avatar(undefined).build();
-      const expected = new ZUrlBuilder().gravatar(hash, ZProfileAvatarSize).build();
-      const target = createTestTarget();
-      // Act
-      const actual = await target.getAvatar(profile);
-      // Assert
-      expect(actual).toEqual(expected);
-    });
-
-    it('should return the profiles avatar if it is set.', async () => {
-      // Arrange
-      const target = createTestTarget();
-      // Act
-      const actual = await target.getAvatar(profile);
-      // Assert
-      expect(actual).toEqual(avatar);
-    });
-  });
-
-  describe('Display', () => {
-    it('should return the display text if the display is set.', async () => {
-      // Arrange
-      const target = createTestTarget();
-      // Act
-      const actual = await target.getDisplay(profile);
-      // Assert
-      expect(actual).toEqual(profile.display);
-    });
-
-    it('should return the email text if the email is set but display is not.', async () => {
-      // Arrange
-      const target = createTestTarget();
-      profile = new ZProfileBuilder().copy(profile).display(undefined).build();
-      // Act
-      const actual = await target.getDisplay(profile);
-      // Assert
-      expect(actual).toEqual(profile.email);
-    });
-
-    it('should display the empty string on a falsy profile.', async () => {
-      // Arrange
-      const target = createTestTarget();
-      // Act
-      const actual = await target.getDisplay(null);
-      // Assert
-      expect(actual).toEqual('');
-    });
-
-    it('should display the empty string if no email or display is set.', async () => {
-      // Arrange
-      const target = createTestTarget();
-      profile = new ZProfileBuilder().build();
-      // Act
-      const actual = await target.getDisplay(profile);
-      // Assert
-      expect(actual).toEqual('');
     });
   });
 });
