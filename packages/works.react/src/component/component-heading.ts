37e0a3ec4f1fd1fdb2120cc07f24cbc051456605
Anthony Bonta <zthun.coding@gmail.com>
2022-10-17 16:46:28 -0500

    feat: added heading component

---
+++ packages/works.react/src/component/component-heading.ts
@@ -0,0 +1,15 @@
+import { ReactNode } from 'react';
+
+/**
+ * A component that contains a heading.
+ */
+export interface IZComponentHeading {
+  /**
+   * The root heading.
+   */
+  heading?: ReactNode;
+  /**
+   * The sub heading that further describes the heading.
+   */
+  subHeading?: ReactNode;
+}
