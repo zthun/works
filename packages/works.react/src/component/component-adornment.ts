40c86b870765ff51e96aacbf3804e1d1a9f460c5
Anthony Bonta <zthun.coding@gmail.com>
2022-10-17 16:52:18 -0500

    feat: added an adornment component

---
+++ packages/works.react/src/component/component-adornment.ts
@@ -0,0 +1,8 @@
+import { ReactNode } from 'react';
+
+/**
+ * A component that contains an optional adornment in addition to the main content.
+ */
+export interface IZComponentAdornment {
+  adornment?: ReactNode;
+}
