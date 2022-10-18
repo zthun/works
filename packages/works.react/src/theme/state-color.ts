9efac297d345b6f5b6a1d7bf0aafb28f276d3b5a
Anthony Bonta <zthun.coding@gmail.com>
2022-10-18 00:16:29 -0500

    refactor: state color is now divided into different types

--- packages/works.react/src/theme/state-color.ts
+++ packages/works.react/src/theme/state-color.ts
@@ -1,9 +1,54 @@
-export enum ZStateColor {
+export enum ZSeverityColor {
   Primary = 'primary',
   Secondary = 'secondary',
   Success = 'success',
   Warning = 'warning',
   Error = 'error',
-  Inherit = 'inherit',
   Info = 'info'
 }
+
+export enum ZColorless {
+  Inherit = 'inherit',
+  Transparent = 'transparent'
+}
+
+export enum ZHueColor {
+  Red = 'red',
+  Pink = 'pink',
+  Purple = 'purple',
+  Violet = 'violet',
+  Indigo = 'indigo',
+  Blue = 'blue',
+  Sky = 'sky',
+  Cyan = 'cyan',
+  Teal = 'teal',
+  Green = 'green',
+  tbd = 'green-light',
+  Lime = 'lime',
+  Yellow = 'yellow',
+  Amber = 'amber',
+  Orange = 'orange',
+  Persimmon = 'persimmon',
+  Brown = 'brown'
+}
+
+export enum ZShadeColor {
+  Grey = 'grey',
+  Black = 'black',
+  White = 'white'
+}
+
+export enum ZColorTint {
+  T50 = 50,
+  T100 = 100,
+  T200 = 200,
+  T300 = 300,
+  T400 = 400,
+  T500 = 500,
+  T600 = 600,
+  T700 = 700,
+  T800 = 800,
+  T900 = 900
+}
+
+export type ZStateColor = ZSeverityColor | ZHueColor | ZShadeColor | ZColorless;
