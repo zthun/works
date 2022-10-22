55b4faa6f39c3738235f823b4d661946836e7110
Anthony Bonta <zthun.coding@gmail.com>
2022-10-21 22:55:01 -0500

    build: update exports

--- packages/works.cirque-du-react/src/index.ts
+++ packages/works.cirque-du-react/src/index.ts
@@ -2,9 +2,5 @@
 
 // Driver
 export { ZCircusDriver } from './driver/circus-driver';
-// Performance
-export { ZCircusPerformer } from './performance/circus-performer';
-export { ZCircusWait } from './performance/circus-wait';
 // Setup
-export { ZCircusSetupRender } from './setup/circus-setup-render';
 export { ZCircusSetupRenderer } from './setup/circus-setup-renderer';
--- packages/works.cirque-du-selenium/src/index.ts
+++ packages/works.cirque-du-selenium/src/index.ts
@@ -1,1 +1,4 @@
+// Driver
+export { ZCircusDriver } from './driver/circus-driver';
+// Setup
 export { ZCircusSetupChrome } from './setup/circus-setup-chrome';
