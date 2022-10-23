7a54b4c2c9cd8f37fb82f17ead75c24ea3092c7b
Anthony Bonta <zthun.coding@gmail.com>
2022-10-23 00:48:12 -0500

    refactor!: remove <ZHttpCodeCard />

---
+++ packages/works.react/src/http/status-code-page.tsx
@@ -0,0 +1,87 @@
+import CheckCircleIcon from '@mui/icons-material/CheckCircle';
+import EmojiFoodBeverageTwoToneIcon from '@mui/icons-material/EmojiFoodBeverageTwoTone';
+import ErrorIcon from '@mui/icons-material/Error';
+import ForwardIcon from '@mui/icons-material/Forward';
+import InfoIcon from '@mui/icons-material/Info';
+import WarningIcon from '@mui/icons-material/Warning';
+import {
+  getHttpCodeCategory,
+  getHttpCodeDescription,
+  getHttpCodeName,
+  ZHttpCode,
+  ZHttpCodeClient
+} from '@zthun/works.http';
+import React from 'react';
+import { ZPaperCard } from '../card/paper-card';
+import { ZGridLayout } from '../layout/grid-layout';
+import { useParams } from '../router/router-dom';
+import { ZH2, ZSubtitle } from '../typography/typography';
+
+/**
+ * Represents properties for the status code page.
+ */
+export interface IZStatusCodePageProps {
+  /**
+   * The name of the route param that contains the code.
+   */
+  name: string;
+}
+
+/**
+ * Renders a page that displays a status code card.
+ *
+ * @param props The properties for this component.
+ *
+ * @returns The jsx that renders the status code page.
+ */
+export function ZStatusCodePage(props: IZStatusCodePageProps) {
+  const params = useParams();
+  const code: ZHttpCode = +(params[props.name] || ZHttpCodeClient.ImATeapot);
+
+  const name = getHttpCodeName(code);
+  const heading = getHttpCodeCategory(code);
+  const description = getHttpCodeDescription(code);
+
+  const renderAvatar = () => {
+    if (code === ZHttpCodeClient.ImATeapot) {
+      return (
+        <EmojiFoodBeverageTwoToneIcon
+          className='ZStatusCodePage-icon ZStatusCodePage-teapot'
+          fontSize='inherit'
+          color='warning'
+        />
+      );
+    }
+
+    if (code >= 100 && code < 200) {
+      return <InfoIcon className='ZStatusCodePage-icon ZStatusCodePage-info' fontSize='inherit' color='info' />;
+    }
+
+    if (code >= 200 && code < 300) {
+      return (
+        <CheckCircleIcon className='ZStatusCodePage-icon ZStatusCodePage-success' fontSize='inherit' color='success' />
+      );
+    }
+
+    if (code >= 300 && code < 400) {
+      return (
+        <ForwardIcon className='ZStatusCodePage-icon ZStatusCodePage-redirect' fontSize='inherit' color='success' />
+      );
+    }
+
+    if (code >= 400 && code < 500) {
+      return <WarningIcon className='ZStatusCodePage-icon ZStatusCodePage-client' fontSize='inherit' color='warning' />;
+    }
+
+    return <ErrorIcon className='ZStatusCodePage-icon ZStatusCodePage-server' fontSize='inherit' color='error' />;
+  };
+
+  return (
+    <ZGridLayout className='ZStatusCodePage-root' justifyContent='center'>
+      <ZPaperCard avatar={renderAvatar()} headerText={heading} subHeaderText={name} size='lg'>
+        <ZSubtitle className='ZStatusCodePage-description'>{description}</ZSubtitle>
+        <ZH2 className='ZStatusCodePage-code'>{code}</ZH2>
+      </ZPaperCard>
+    </ZGridLayout>
+  );
+}
