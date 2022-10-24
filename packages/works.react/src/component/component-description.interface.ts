debe1613fc54043ee1de8752f3f8638a93201996
Anthony Bonta <zthun.coding@gmail.com>
2022-10-24 12:36:50 -0500

    chore: cleanup

--- packages/works.react/src/component/component-description.interface.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Represents a component that has a description.
- */
-export interface IZComponentDescription {
-  /**
-   * The description of the component.
-   */
-  description?: string;
-}
--- packages/works.react/src/component/component-dimensions-2d.ts
+++
@@ -1,4 +0,0 @@
-import { IZComponentHeight } from './component-height';
-import { IZComponentWidth } from './component-width';
-
-export interface IZComponentDimensions2d extends IZComponentWidth, IZComponentHeight {}
