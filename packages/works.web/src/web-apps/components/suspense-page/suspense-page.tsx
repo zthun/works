b2b6847b5af89dcdef171d157b648161fb01115e
Anthony Bonta <zthun.coding@gmail.com>
2022-10-25 17:47:50 -0500

    chore: typography

--- packages/works.web/src/web-apps/components/suspense-page/suspense-page.tsx
+++ packages/works.web/src/web-apps/components/suspense-page/suspense-page.tsx
@@ -10,6 +10,7 @@ import {
   ZGridLayout,
   ZH3,
   ZHueColor,
+  ZPaddedBox,
   ZParagraph,
   ZSeverityColor,
   ZShadeColor,
@@ -47,49 +48,54 @@ export function ZSuspensePage() {
       subHeading='Spinners that spin'
       avatar={<LoopIcon color='warning' fontSize='inherit' />}
     >
-      <ZH3>Description</ZH3>
+      <ZPaddedBox margin={{ bottom: ZSizeFixed.Large }}>
+        <ZH3>Description</ZH3>
 
-      <ZParagraph>
-        If you load data from a server, or have to have the user wait for an operation to complete, then the appropriate
-        display for that is some kind of animated, work, icon. These types of displays have very little configuration
-        and are generally nothing more than to show users that things are still happening in the background. However,
-        psychologically, they alleviate any user suspense when asynchronous operations are happening.
-      </ZParagraph>
+        <ZParagraph>
+          If you load data from a server, or have to have the user wait for an operation to complete, then the
+          appropriate display for that is some kind of animated, work, icon. These types of displays have very little
+          configuration and are generally nothing more than to show users that things are still happening in the
+          background. However, psychologically, they alleviate any user suspense when asynchronous operations are
+          happening.
+        </ZParagraph>
 
-      <ZSuspenseRotate loading={loading} color={color} tint={tint} width={width} />
+        <ZSuspenseRotate loading={loading} color={color} tint={tint} width={width} />
+      </ZPaddedBox>
 
-      <ZH3>Options</ZH3>
+      <ZPaddedBox margin={{ bottom: ZSizeFixed.Large }}>
+        <ZH3>Options</ZH3>
 
-      <ZGridLayout gap={ZSizeFixed.Medium}>
-        <ZBooleanSwitch value={loading} onValueChange={setLoading} label='Loading' />
-        <ZChoiceDropDown
-          name='width'
-          value={[width]}
-          onValueChange={_setWidth}
-          options={sizes}
-          renderOption={startCase}
-          identifier={identity}
-          label='Width'
-        />
-        <ZChoiceDropDown
-          name='color'
-          value={[color]}
-          onValueChange={_setColor}
-          options={colors}
-          renderOption={startCase}
-          identifier={identity}
-          label='Color'
-        />
-        <ZChoiceDropDown
-          name='tint'
-          value={[tint]}
-          onValueChange={_setTint}
-          options={tints}
-          renderOption={startCase}
-          identifier={identity}
-          label='Tint'
-        />
-      </ZGridLayout>
+        <ZGridLayout gap={ZSizeFixed.Medium}>
+          <ZBooleanSwitch value={loading} onValueChange={setLoading} label='Loading' />
+          <ZChoiceDropDown
+            name='width'
+            value={[width]}
+            onValueChange={_setWidth}
+            options={sizes}
+            renderOption={startCase}
+            identifier={identity}
+            label='Width'
+          />
+          <ZChoiceDropDown
+            name='color'
+            value={[color]}
+            onValueChange={_setColor}
+            options={colors}
+            renderOption={startCase}
+            identifier={identity}
+            label='Color'
+          />
+          <ZChoiceDropDown
+            name='tint'
+            value={[tint]}
+            onValueChange={_setTint}
+            options={tints}
+            renderOption={startCase}
+            identifier={identity}
+            label='Tint'
+          />
+        </ZGridLayout>
+      </ZPaddedBox>
     </ZCard>
   );
 }
