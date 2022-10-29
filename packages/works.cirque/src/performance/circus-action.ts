0edfc76cc3b465dd03e083b266fe3688f32d2839
Anthony Bonta <zthun.coding@gmail.com>
2022-10-29 14:11:28 -0500

    chore: add support to determine type of action

--- packages/works.cirque/src/performance/circus-action.ts
+++ packages/works.cirque/src/performance/circus-action.ts
@@ -15,3 +15,40 @@ export interface IZCircusAction<TContext = any> {
   name: ZCircusActionType;
   context: TContext;
 }
+
+/**
+ * Gets whether an action represents a keyboard action.
+ *
+ * @param action
+ *        The action or action type to check.
+ *
+ * @returns
+ *        True if the action represents a key down or key up event.
+ *        False otherwise.
+ */
+export function isKeyboardAction(action: IZCircusAction) {
+  const { name } = action;
+  return name === ZCircusActionType.KeyDown || name === ZCircusActionType.KeyUp;
+}
+
+/**
+ * Gets whether an action represents a mouse action.
+ *
+ * @param action
+ *        The action or action type to check.
+ *
+ * @returns
+ *        True if the action represents a mouse event.
+ */
+export function isMouseAction(action: IZCircusAction) {
+  const { name } = action;
+
+  return (
+    [
+      ZCircusActionType.MouseLeftDown,
+      ZCircusActionType.MouseRightDown,
+      ZCircusActionType.MouseLeftUp,
+      ZCircusActionType.MouseRightUp
+    ].indexOf(name) >= 0
+  );
+}
