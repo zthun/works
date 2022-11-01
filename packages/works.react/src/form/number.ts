31eade3cb0abe06d861c31a8710e0fbe3d074783
Anthony Bonta <zthun.coding@gmail.com>
2022-11-01 16:05:24 -0500

    refactor: remove width support from slider

--- packages/works.react/src/form/number.ts
+++ packages/works.react/src/form/number.ts
@@ -1,11 +1,9 @@
-import { ZSizeFixed, ZSizeVaried } from '@zthun/works.chonky-cat';
 import { IZComponentDisabled } from '../component/component-disabled';
 import { IZComponentLabel } from '../component/component-label';
 import { IZComponentName } from '../component/component-name';
 import { IZComponentRange } from '../component/component-range';
 import { IZComponentStyle } from '../component/component-style.';
 import { IZComponentValue } from '../component/component-value';
-import { IZComponentWidth } from '../component/component-width';
 
 /**
  * Represents a component that lets the user enter or select a number.
@@ -16,7 +14,6 @@ export interface IZNumber<T = number>
     IZComponentRange<number>,
     IZComponentName,
     IZComponentLabel,
-    IZComponentWidth<ZSizeFixed | ZSizeVaried.Full>,
     IZComponentStyle {
   step?: number;
 }
