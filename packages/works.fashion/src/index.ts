0c56e8bdc1823fe719b92300037ad0e6958f584e
Anthony Bonta <zthun.coding@gmail.com>
2022-11-08 16:04:12 -0600

    build: export renamed files

--- packages/works.fashion/src/index.ts
+++ packages/works.fashion/src/index.ts
@@ -1,7 +1,7 @@
 /* istanbul ignore file */
 
 export { IZFashion, ZFashionBuilder } from './color/fashion';
-export { IZFashionComplements, ZFashionComplementsBuilder } from './color/fashion-complements';
+export { IZFashionCoordination, ZFashionCoordinationBuilder } from './color/fashion-coordination';
 export { ZHue } from './color/hue';
-export { ZPalette, ZPaletteBuilder } from './color/palette';
+export { IZPalette, ZPaletteBuilder } from './color/palette';
 export { ZLuminance, ZShade, ZShades } from './color/shade';
