f38f1741676e64b138fc5729a7b364013902042e
Anthony Bonta <zthun.coding@gmail.com>
2022-11-08 23:14:14 -0600

    refactor: size should now properly calculate

--- packages/works.react/src/suspense/suspense-rotate.tsx
+++ packages/works.react/src/suspense/suspense-rotate.tsx
@@ -2,22 +2,9 @@ import { CircularProgress } from '@mui/material';
 import { createSizeChartFixedArithmetic, createSizeChartFixedCss, ZSizeFixed } from '@zthun/works.chonky-cat';
 import { cssClass } from '@zthun/works.core';
 import React from 'react';
-import { makeStyles } from '../theme/make-styles';
 import { IZSuspense } from './suspense';
 
 const SuspenseRotateSizeChart = createSizeChartFixedCss(createSizeChartFixedArithmetic(1, 1), 'rem');
-
-const useSuspenseRotateStyles = makeStyles<IZSuspense>()((theme, props) => {
-  const { width = ZSizeFixed.ExtraSmall } = props;
-  const size = SuspenseRotateSizeChart[width];
-
-  return {
-    root: {
-      height: size,
-      width: size
-    }
-  };
-});
 
 /**
  * Renders a circular progress that can render nothing or the material ui circular progress.
@@ -27,14 +14,13 @@ const useSuspenseRotateStyles = makeStyles<IZSuspense>()((theme, props) => {
  * @returns The jsx for a circular loading progress.
  */
 export function ZSuspenseRotate(props: IZSuspense) {
-  const { className, loading = true, name } = props;
-  const { classes } = useSuspenseRotateStyles(props);
+  const { className, loading = true, name, width = ZSizeFixed.ExtraSmall } = props;
+  const size = SuspenseRotateSizeChart[width];
 
   if (!loading) {
     return null;
   }
 
-  const clasz = cssClass('ZSuspense-root ZSuspense-rotate', className, classes.root);
-
-  return <CircularProgress className={clasz} size='inherit' color='inherit' data-name={name} />;
+  const clasz = cssClass('ZSuspense-root ZSuspense-rotate', className);
+  return <CircularProgress className={clasz} size={size} color='inherit' data-name={name} />;
 }
