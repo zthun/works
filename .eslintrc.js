d9d12ec218aed6b370f2cadf2cba442d446d11fd
Anthony Bonta <zthun.coding@gmail.com>
2022-11-13 17:24:14 -0600

    chore: let me do non null assertions

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,4 @@
 const config = require('@zthun/lint-janitor-config/eslint-react');
 config.rules['@typescript-eslint/no-empty-interface'] = 'off';
+config.rules['@typescript-eslint/no-non-null-assertion'] = 'off';
 module.exports = config;
