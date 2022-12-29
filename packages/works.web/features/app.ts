0a782a7664f3458c60d8b83bdf8a71e41eb0029b
Anthony Bonta <zthun.coding@gmail.com>
2022-12-28 22:44:15 -0600

    test: added a generic ok loads successfully check

--- packages/works.web/features/app.ts
+++ packages/works.web/features/app.ts
@@ -1,8 +1,13 @@
-import { After, Given } from '@cucumber/cucumber';
+import { After, Given, Then } from '@cucumber/cucumber';
+import assert from 'assert';
 import { ZLearnWorld } from './learn-world';
 
 Given<ZLearnWorld>('I open the learn application', async function (this: ZLearnWorld) {
   await this.open();
+});
+
+Then('the page loads successfully', async function (this: ZLearnWorld) {
+  assert.ok(true);
 });
 
 After(async function (this: ZLearnWorld) {
