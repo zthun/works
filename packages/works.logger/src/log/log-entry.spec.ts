9dea48293fb60cd84180d8e137f61a1b4ac942dc
Anthony Bonta <zthun.coding@gmail.com>
2023-01-03 09:59:50 -0600

    refactor: removed dependency on works.jest

--- packages/works.logger/src/log/log-entry.spec.ts
+++ packages/works.logger/src/log/log-entry.spec.ts
@@ -1,6 +1,5 @@
 /* eslint-disable require-jsdoc */
-import { assertBuilderSetsProperty } from '@zthun/works.jest';
-import { IZLogEntry, ZLogEntryBuilder, ZLogLevel } from './log-entry';
+import { ZLogEntryBuilder, ZLogLevel } from './log-entry';
 
 describe('ZLogEntryBuilder', () => {
   function createTestTarget() {
@@ -9,48 +8,24 @@ describe('ZLogEntryBuilder', () => {
 
   describe('Properties', () => {
     it('should set the log level to catastrophe.', () => {
-      assertBuilderSetsProperty(
-        ZLogLevel.CATASTROPHE,
-        createTestTarget,
-        (t) => t.info().catastrophe(),
-        (l: IZLogEntry) => l.level
-      );
+      expect(createTestTarget().info().catastrophe().build().level).toEqual(ZLogLevel.CATASTROPHE);
     });
 
     it('should set the log level to error.', () => {
-      assertBuilderSetsProperty(
-        ZLogLevel.ERROR,
-        createTestTarget,
-        (t) => t.info().error(),
-        (l: IZLogEntry) => l.level
-      );
+      expect(createTestTarget().info().error().build().level).toEqual(ZLogLevel.ERROR);
     });
 
     it('should set the log level to warning.', () => {
-      assertBuilderSetsProperty(
-        ZLogLevel.WARNING,
-        createTestTarget,
-        (t) => t.info().warning(),
-        (l: IZLogEntry) => l.level
-      );
+      expect(createTestTarget().info().warning().build().level).toEqual(ZLogLevel.WARNING);
     });
 
     it('should set the log level to info.', () => {
-      assertBuilderSetsProperty(
-        ZLogLevel.INFO,
-        createTestTarget,
-        (t) => t.error().info(),
-        (l: IZLogEntry) => l.level
-      );
+      expect(createTestTarget().catastrophe().info().build().level).toEqual(ZLogLevel.INFO);
     });
 
     it('should set the message.', () => {
-      assertBuilderSetsProperty(
-        'Error Message',
-        createTestTarget,
-        (t, v) => t.message(v),
-        (l: IZLogEntry) => l.message
-      );
+      const expected = 'Error Message';
+      expect(createTestTarget().message(expected).build().message).toEqual(expected);
     });
   });
 });
