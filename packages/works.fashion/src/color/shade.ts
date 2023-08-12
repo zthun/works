4bef527e1b2db743d1c8f03202de51207647153f
Anthony Bonta <zthun.coding@gmail.com>
2023-08-12 11:17:03 -0500

    feat!: remove works.fashion in favor of fashion-theme and fashion-boutique

--- packages/works.fashion/src/color/shade.ts
+++
@@ -1,6 +0,0 @@
-export type ZShade = 50 | 100 | 200 | 300 | 400 | 500 | 600 | 700 | 800 | 900;
-
-/**
- * All supported shades in an array.
- */
-export const ZShades: Readonly<ZShade[]> = Object.freeze([50, 100, 200, 300, 400, 500, 600, 700, 800, 900]);
