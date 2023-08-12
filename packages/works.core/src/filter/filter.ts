9425b970d6d50b8c4858596aba7d032367f5166c
Anthony Bonta <zthun.coding@gmail.com>
2023-08-12 10:52:45 -0500

    chore: cleanup

--- packages/works.core/src/filter/filter.ts
+++
@@ -1,9 +0,0 @@
-import { IZBinaryFilter } from './binary-filter';
-import { IZCollectionFilter } from './collection-filter';
-import { IZLogicFilter } from './logic-filter';
-import { IZUnaryFilter } from './unary-filter';
-
-/**
- * Represents one of the filter types.
- */
-export type IZFilter = IZBinaryFilter | IZLogicFilter | IZCollectionFilter | IZUnaryFilter;
--- packages/works.core/src/index.ts
+++ packages/works.core/src/index.ts
@@ -6,34 +6,15 @@ export { IZWebApp, ZWebAppBuilder } from './app/web-app';
 export { IZConfigEntry, ZConfigEntryBuilder } from './configs/config-entry';
 // Cookie
 export { IZCookie, ZCookieBuilder } from './cookie/cookie';
-// Doc
-export { IZTypedocCommentTag } from './doc/typedoc-comment-tag.interface';
-export { IZTypedocComment } from './doc/typedoc-comment.interface';
-export { IZTypedocEntity } from './doc/typedoc-entity.interface';
-export { IZTypedocFlags } from './doc/typedoc-flags.interface';
-export { IZTypedocGroup } from './doc/typedoc-group.interface';
-export { ZTypedocKind } from './doc/typedoc-kind.enum';
-export { IZTypedocSource } from './doc/typedoc-source.interface';
-export { ZTypedocTypeKind } from './doc/typedoc-type-kind.enum';
-export { IZTypedocType } from './doc/typedoc-type.interface';
-export { IZTypedoc } from './doc/typedoc.interface';
 // Email
 export { IZEmail, ZEmailBuilder } from './email/email';
 export { IZEmailContact, ZEmailContactBuilder } from './email/email-contact';
 export { ZEmailContactAddressBuilder } from './email/email-contact-address';
 export { IZEmailEnvelope, ZEmailEnvelopeBuilder } from './email/email-envelope';
-// Filter
-export { IZBinaryFilter, ZBinaryFilterBuilder, ZBinaryOperator } from './filter/binary-filter';
-export { IZCollectionFilter, ZCollectionFilterBuilder, ZCollectionOperator } from './filter/collection-filter';
-export { IZFilter } from './filter/filter';
-export { IZLogicFilter, ZLogicFilterBuilder, ZLogicOperator } from './filter/logic-filter';
-export { IZUnaryFilter, ZUnaryFilterBuilder, ZUnaryOperator } from './filter/unary-filter';
 // Route
 export { IZRouteOption, ZRouteOptionBuilder } from './route/route-option';
 // Server
 export { IZServer, ZServerBuilder } from './server/server';
-// Sort
-export { IZSort, ZSortBuilder, ZSortDirection } from './sort/sort';
 // User
 export { IZLogin, ZLoginBuilder } from './users/login';
 export { IZProfile, ZProfileAvatarMaxBytes, ZProfileAvatarSize, ZProfileBuilder } from './users/profile';
