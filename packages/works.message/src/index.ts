8028621a56ae24380882af3cbaab7bcec37fa7c1
Anthony Bonta <zthun.coding@gmail.com>
2023-09-02 15:35:46 -0500

    refactor!: removed works.message as alerts are now in fashion

--- packages/works.message/src/index.ts
+++
@@ -1,5 +0,0 @@
-/* istanbul ignore file */
-
-// Alert
-export { IZAlert, ZAlertBuilder, ZAlertSeverity } from './alert/alert';
-export { IZAlertService, ZAlertService } from './alert/alert-service';
