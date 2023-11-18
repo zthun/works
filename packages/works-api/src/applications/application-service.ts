56249170364248d29e79d885a768fdedc1d23dbd
Anthony Bonta <zthun.coding@gmail.com>
2023-11-17 22:08:13 -0600

    chore: implement read

--- packages/works-api/src/applications/application-service.ts
+++ packages/works-api/src/applications/application-service.ts
@@ -1,5 +1,13 @@
-import { Inject, Injectable } from '@nestjs/common';
-import { IZDataRequest, IZDataSource, IZPage, ZDataSourceStatic, ZPageBuilder } from '@zthun/helpful-query';
+import { Inject, Injectable, NotFoundException } from '@nestjs/common';
+import {
+  IZDataRequest,
+  IZDataSource,
+  IZPage,
+  ZDataRequestBuilder,
+  ZDataSourceStatic,
+  ZFilterBinaryBuilder,
+  ZPageBuilder
+} from '@zthun/helpful-query';
 import { IZVaultClient, ZVaultToken } from '@zthun/vault-client';
 import { IZApplication } from './application';
 
@@ -7,6 +15,8 @@ export const ZApplicationsToken = Symbol();
 
 export interface IZApplicationsService {
   list(request: IZDataRequest): Promise<IZPage<IZApplication>>;
+
+  read(id: string): Promise<IZApplication>;
 }
 
 @Injectable()
@@ -20,6 +30,19 @@ export class ZApplicationsService implements IZApplicationsService {
     const apps = await source.retrieve(request);
     const count = await source.count(request);
     return new ZPageBuilder<IZApplication>().data(apps).count(count).build();
+  }
+
+  public async read(id: string): Promise<IZApplication> {
+    const source = await this._load();
+    const byId = new ZFilterBinaryBuilder().subject('_id').equal().value(id).build();
+    const request = new ZDataRequestBuilder().filter(byId).page(1).size(1).build();
+    const [application] = await source.retrieve(request);
+
+    if (application == null) {
+      return Promise.reject(new NotFoundException(`No project with id, ${id}, exists`));
+    }
+
+    return application;
   }
 
   private async _load(): Promise<IZDataSource<IZApplication>> {
