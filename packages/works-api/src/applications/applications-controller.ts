e1baba729368ed47589257d697e729662a6ddc60
Anthony Bonta <zthun.coding@gmail.com>
2023-11-17 22:02:22 -0600

    refactor: get endpoint

--- packages/works-api/src/applications/applications-controller.ts
+++ packages/works-api/src/applications/applications-controller.ts
@@ -1,15 +1,20 @@
-import { Controller, Get, Inject, Query } from '@nestjs/common';
+import { Controller, Get, Inject, Param, Query } from '@nestjs/common';
 import { IZDataRequestQuery, IZPage, ZDataRequestBuilder } from '@zthun/helpful-query';
 import { IZApplication } from './application';
 import { IZApplicationsService, ZApplicationsToken } from './application-service';
 
-@Controller()
+@Controller('applications')
 export class ZApplicationsController {
   public constructor(@Inject(ZApplicationsToken) private readonly _apps: IZApplicationsService) {}
 
-  @Get('applications')
+  @Get()
   public list(@Query() query: IZDataRequestQuery): Promise<IZPage<IZApplication>> {
     const request = new ZDataRequestBuilder().query(query).build();
     return this._apps.list(request);
+  }
+
+  @Get(':id')
+  public read(@Param('id') id: string): Promise<IZApplication> {
+    return this._apps.read(id);
   }
 }
