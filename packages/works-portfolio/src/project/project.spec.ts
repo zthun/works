9d5b88943428da5dee2842bd471fa14c3a7df6a5
Anthony Bonta <zthun.coding@gmail.com>
2023-11-19 10:00:29 -0600

    feat: portfolio projects describe applications and projects

---
+++ packages/works-portfolio/src/project/project.spec.ts
@@ -0,0 +1,116 @@
+import { createGuid } from '@zthun/helpful-fn';
+import { ZUrlBuilder } from '@zthun/webigail-url';
+import { describe, expect, it } from 'vitest';
+import { IZProject, ZProjectBuilder, ZProjectKind, ZProjectTechnology } from './project';
+
+describe('ZProjectBuilder', () => {
+  const createTestTarget = () => new ZProjectBuilder();
+
+  describe('Id', () => {
+    it('should set the value', () => {
+      const expected = createGuid();
+      expect(createTestTarget().id(expected).build()._id).toEqual(expected);
+    });
+  });
+
+  describe('Name', () => {
+    it('should set the value', () => {
+      const expected = 'Zthunworks';
+      expect(createTestTarget().name(expected).build().name).toEqual(expected);
+    });
+  });
+
+  describe('Icon', () => {
+    it('should set the value', () => {
+      const expected = new ZUrlBuilder().gravatar().build();
+      expect(createTestTarget().icon(expected).build().icon).toEqual(expected);
+    });
+  });
+
+  describe('Url', () => {
+    it('should set the value', () => {
+      const expected = 'https://google.com';
+      expect(createTestTarget().url(expected).build().url).toEqual(expected);
+    });
+  });
+
+  describe('Source', () => {
+    it('should set the value', () => {
+      const expected = new ZUrlBuilder().parse('https://github.com').append('zthun').append('works').build();
+      expect(createTestTarget().source(expected).build().source).toEqual(expected);
+    });
+  });
+
+  describe('Kind', () => {
+    it('should default to other', () => {
+      expect(createTestTarget().build().kind).toEqual(ZProjectKind.Other);
+    });
+
+    it('should set to library', () => {
+      expect(createTestTarget().library().build().kind).toEqual(ZProjectKind.Library);
+    });
+
+    it('should set to web', () => {
+      expect(createTestTarget().web().build().kind).toEqual(ZProjectKind.Web);
+    });
+
+    it('should set to desktop', () => {
+      expect(createTestTarget().desktop().build().kind).toEqual(ZProjectKind.Desktop);
+    });
+
+    it('should set to console', () => {
+      expect(createTestTarget().console().build().kind).toEqual(ZProjectKind.Console);
+    });
+  });
+
+  describe('Technology', () => {
+    it('should default to other', () => {
+      expect(createTestTarget().build().technology).toEqual(ZProjectTechnology.Other);
+    });
+
+    it('should set to java', () => {
+      expect(createTestTarget().java().build().technology).toEqual(ZProjectTechnology.Java);
+    });
+
+    it('should set to dotnet', () => {
+      expect(createTestTarget().dotnet().build().technology).toEqual(ZProjectTechnology.DotNet);
+    });
+
+    it('should set to node', () => {
+      expect(createTestTarget().node().build().technology).toEqual(ZProjectTechnology.Node);
+    });
+  });
+
+  describe('Assign', () => {
+    it('should assign updated properties', () => {
+      // Arrange.
+      const target = createTestTarget();
+      const expected = target.name('Test').java().console().build();
+      const partial: Partial<IZProject> = { name: expected.name, kind: expected.kind, technology: expected.technology };
+      // Act.
+      const actual = target.assign(partial).build();
+      // Assert.
+      expect(actual).toEqual(expected);
+    });
+  });
+
+  describe('Copy', () => {
+    it('should create a deep copy of the assigned project', () => {
+      // Arrange.
+      const target = createTestTarget();
+      const expected = target
+        .id('test')
+        .name('Test')
+        .java()
+        .console()
+        .icon(new ZUrlBuilder().gravatar().build())
+        .url('https://zthunworks.com')
+        .source('https://github.com/zthun/works')
+        .build();
+      // Act.
+      const actual = target.copy(expected).build();
+      // Assert.
+      expect(actual).toEqual(expected);
+    });
+  });
+});
---
+++ packages/works-portfolio/src/project/project.ts
@@ -0,0 +1,95 @@
+export enum ZProjectTechnology {
+  Other = 'other',
+  Node = 'node',
+  Java = 'java',
+  DotNet = 'dotnet'
+}
+
+export enum ZProjectKind {
+  Other = 'other',
+  Library = 'library',
+  Web = 'web',
+  Desktop = 'desktop',
+  Console = 'console'
+}
+
+export interface IZProject {
+  _id: string;
+  name: string;
+  kind: ZProjectKind;
+  technology: ZProjectTechnology;
+  icon?: string;
+  url?: string;
+  source?: string;
+}
+
+export class ZProjectBuilder {
+  private _project: IZProject;
+
+  public constructor() {
+    this._project = {
+      _id: '',
+      name: '',
+      kind: ZProjectKind.Other,
+      technology: ZProjectTechnology.Other
+    };
+  }
+
+  public id(id: string): this {
+    this._project._id = id;
+    return this;
+  }
+
+  public name(name: string): this {
+    this._project.name = name;
+    return this;
+  }
+
+  public kind(kind: ZProjectKind): this {
+    this._project.kind = kind;
+    return this;
+  }
+
+  public library = this.kind.bind(this, ZProjectKind.Library);
+  public desktop = this.kind.bind(this, ZProjectKind.Desktop);
+  public web = this.kind.bind(this, ZProjectKind.Web);
+  public console = this.kind.bind(this, ZProjectKind.Console);
+
+  public technology(technology: ZProjectTechnology): this {
+    this._project.technology = technology;
+    return this;
+  }
+
+  public java = this.technology.bind(this, ZProjectTechnology.Java);
+  public node = this.technology.bind(this, ZProjectTechnology.Node);
+  public dotnet = this.technology.bind(this, ZProjectTechnology.DotNet);
+
+  public icon(icon: string): this {
+    this._project.icon = icon;
+    return this;
+  }
+
+  public url(url: string): this {
+    this._project.url = url;
+    return this;
+  }
+
+  public source(source: string): this {
+    this._project.source = source;
+    return this;
+  }
+
+  public assign(other: Partial<IZProject>) {
+    this._project = { ...this._project, ...other };
+    return this;
+  }
+
+  public copy(other: IZProject) {
+    this._project = structuredClone(other);
+    return this;
+  }
+
+  public build(): IZProject {
+    return this._project;
+  }
+}
