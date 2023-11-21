a2bafed6eab1175e1fd830fef26c8cdef16e3dd5
Anthony Bonta <zthun.coding@gmail.com>
2023-11-21 15:09:16 -0600

    refactor: rename to vitest config

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite';
 import tsConfigPaths from 'vite-tsconfig-paths';
+import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   plugins: [tsConfigPaths()],
