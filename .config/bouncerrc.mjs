c29a64fe25ea8cdd3b5f6fbdd078aa729cb4fdf0
Anthony Bonta <zthun.coding@gmail.com>
2025-07-01 21:27:58 -0500

    chore: cleanup

--- .config/bouncerrc.mjs
+++ .config/bouncerrc.mjs
@@ -1,23 +1,3 @@
-/*
-export default {
-  domains: [
-    {
-      host: "pokedexii.local.zthunworks.com",
-      paths: {
-        "/": "pokedex-services-web:5173",
-        "/api": "pokedex-services-api:3000/api",
-      },
-    },
-    {
-      host: "database.local.zthunworks.com",
-      paths: {
-        "/": "pokedex-mongo-express:8081",
-      },
-    },
-  ],
-};
-*/
-
 export default {
   domains: [
     {
